87d8a64e3ba843cad5f21357d37f59ef35af33fa
SeongJae Park <sj38.park@gmail.com>
2021-04-02 08:06:49 +0000

    bin2txt: Use the record file parsing module
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- bin2txt.py
+++ bin2txt.py
@@ -3,25 +3,9 @@
 
 import argparse
 import os
-import struct
 import sys
 
-import _recfile
-
-def pr_region(f):
-    saddr = struct.unpack('L', f.read(8))[0]
-    eaddr = struct.unpack('L', f.read(8))[0]
-    nr_accesses = struct.unpack('I', f.read(4))[0]
-    print("%012x-%012x(%10d):\t%d" %
-            (saddr, eaddr, eaddr - saddr, nr_accesses))
-
-def pr_task_info(f):
-    target_id = _recfile.target_id(f)
-    print("target_id: ", target_id)
-    nr_regions = struct.unpack('I', f.read(4))[0]
-    print("nr_regions: ", nr_regions)
-    for r in range(nr_regions):
-        pr_region(f)
+import _read_record
 
 def set_argparser(parser):
     parser.add_argument('--input', '-i', type=str, metavar='<file>',
@@ -39,23 +23,19 @@ def main(args=None):
         print('input file (%s) is not exist' % file_path)
         exit(1)
 
-    with open(file_path, 'rb') as f:
-        _recfile.set_fmt_version(f)
-        start_time = None
-        while True:
-            timebin = f.read(16)
-            if len(timebin) != 16:
-                break
-            time = _recfile.parse_time(timebin)
-            if not start_time:
-                start_time = time
-                print("start_time: ", start_time)
-            print("rel time: %16d" % (time - start_time))
-            nr_tasks = struct.unpack('I', f.read(4))[0]
-            print("nr_tasks: ", nr_tasks)
-            for t in range(nr_tasks):
-                pr_task_info(f)
-                print("")
+    record = _read_record.read_record(file_path)
+    if not record:
+        print('no record in the file')
+    print('start_time: ', record.start_time)
+    for snapshot in record.snapshots:
+        print('rel time: %16d' % (snapshot.monitored_time - record.start_time))
+        print('nr_tasks:  1')
+        print('target_id: ', snapshot.target_id)
+        print('nr_regions: ', len(snapshot.regions))
+        for r in snapshot.regions:
+            print("%012x-%012x(%10d):\t%d" %
+                    (r.start, r.end, r.end - r.start, r.nr_accesses))
+        print()
 
 if __name__ == '__main__':
     main()
