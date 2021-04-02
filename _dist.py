b95b3002e8cad26c4943c13595e79bbcf5c975b2
SeongJae Park <sj38.park@gmail.com>
2021-04-02 08:59:07 +0000

    _dist: Remove unused functions and classes
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _dist.py
+++ _dist.py
@@ -5,27 +5,6 @@ import os
 import struct
 import subprocess
 
-class Region:
-    start = None
-    end = None
-    nr_accesses = None
-
-    def __init__(self, start, end, nr_accesses):
-        self.start = start
-        self.end = end
-        self.nr_accesses = nr_accesses
-
-def access_patterns(f):
-    nr_regions = struct.unpack('I', f.read(4))[0]
-
-    patterns = []
-    for r in range(nr_regions):
-        saddr = struct.unpack('L', f.read(8))[0]
-        eaddr = struct.unpack('L', f.read(8))[0]
-        nr_accesses = struct.unpack('I', f.read(4))[0]
-        patterns.append(Region(saddr, eaddr, nr_accesses))
-    return patterns
-
 def plot_dist(data_file, output_file, xlabel, ylabel):
     terminal = output_file.split('.')[-1]
     if not terminal in ['pdf', 'jpeg', 'png', 'svg']:
