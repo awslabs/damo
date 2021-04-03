167dd0494066a84cb1e81d960c16976f101e659f
SeongJae Park <sj38.park@gmail.com>
2021-04-03 08:13:02 +0000

    tests/test: Print out passed test
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/test.sh
+++ tests/test.sh
@@ -10,6 +10,7 @@ then
 	echo "report-raw FAIL"
 	exit 1
 fi
+echo "report-raw PASS"
 
 ../damo report raw --perf_script -i perf_script_output \
 	> raw_perf_script_after
@@ -19,6 +20,7 @@ then
 	echo "report-raw --perf_script FAIL"
 	exit 1
 fi
+echo "report-raw --perf_script PASS"
 
 ../damo report wss -r 1 101 1 > wss_after
 diff wss_before wss_after
@@ -27,6 +29,7 @@ then
 	echo "report-wss FAIL"
 	exit 1
 fi
+echo "report-wss PASS"
 
 ../adjust.py 1000000
 ../damo report raw -i damon.adjusted.data > aggr_1s_raw_after
@@ -36,6 +39,7 @@ then
 	echo "adjust FAIL"
 	exit 1
 fi
+echo "adjust PASS"
 
 ../damo report nr_regions -r 1 101 1 > nr_regions_after
 diff nr_regions_before nr_regions_after
@@ -44,6 +48,7 @@ then
 	echo "report-nr_regions FAIL"
 	exit 1
 fi
+echo "report-nr_regions PASS"
 
 ../damo report heats --guide > heats_guide_after
 diff heats_guide_before heats_guide_after
@@ -52,6 +57,7 @@ then
 	echo "report-heats-guide FAIL"
 	exit 1
 fi
+echo "report-heats-guide PASS"
 
 ../damo report heats > heats_after
 diff heats_before heats_after
@@ -60,6 +66,7 @@ then
 	echo "report-heats FAIL"
 	exit 1
 fi
+echo "report-heats PASS"
 
 rm *_after
 
