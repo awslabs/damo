b8239515b5fc9a908fc881001a914f9c3a22afe6
SeongJae Park <sjpark@amazon.de>
2021-04-07 12:20:05 +0200

    _parse_damon_result: Update the results structure
    This commit updates the DAMON results structure.  Snapshots are
    collected per target id, and each snapshot has monitoring start time and
    end time now.
    
    NOTE: This makes the report subcommands and the tests fail.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- _parse_damon_result.py
+++ _parse_damon_result.py
@@ -15,22 +15,22 @@ class DAMONRegion:
         self.nr_accesses = nr_accesses
 
 class DAMONSnapshot:
-    monitored_time = None
+    start_time = None
+    end_time = None
     target_id = None
     regions = None
 
-    def __init__(self, monitored_time, target_id):
-        self.monitored_time = monitored_time
+    def __init__(self, start_time, end_time, target_id):
+        self.start_time = start_time
+        self.end_time = end_time
         self.target_id = target_id
         self.regions = []
 
 class DAMONResult:
-    start_time = None
-    snapshots = None
+    snapshots = None    # {target: [snapshot]}
 
-    def __init__(self, start_time):
-        self.start_time = start_time
-        self.snapshots = []
+    def __init__(self):
+        self.snapshots = {}
 
 def record_to_damon_result(file_path):
     result = None
@@ -44,22 +44,31 @@ def record_to_damon_result(file_path):
             fmt_version = 0
             f.seek(0)
 
+        result = DAMONResult()
+
         while True:
             timebin = f.read(16)
             if len(timebin) != 16:
                 break
             sec = struct.unpack('l', timebin[0:8])[0]
             nsec = struct.unpack('l', timebin[8:16])[0]
-            time = sec * 1000000000 + nsec
-            if not result:
-                result = DAMONResult(time)
+            end_time = sec * 1000000000 + nsec
             nr_tasks = struct.unpack('I', f.read(4))[0]
             for t in range(nr_tasks):
                 if fmt_version == 1:
                     target_id = struct.unpack('i', f.read(4))[0]
                 else:
                     target_id = struct.unpack('L', f.read(8))[0]
-                snapshot = DAMONSnapshot(time, target_id)
+
+                if not target_id in result.snapshots:
+                    result.snapshots[target_id] = []
+                target_snapshots = result.snapshots[target_id]
+                if len(target_snapshots) == 0:
+                    start_time = -1
+                else:
+                    start_time = target_snapshots[-1].end_time
+
+                snapshot = DAMONSnapshot(start_time, end_time, target_id)
                 nr_regions = struct.unpack('I', f.read(4))[0]
                 for r in range(nr_regions):
                     start_addr = struct.unpack('L', f.read(8))[0]
@@ -67,7 +76,7 @@ def record_to_damon_result(file_path):
                     nr_accesses = struct.unpack('I', f.read(4))[0]
                     region = DAMONRegion(start_addr, end_addr, nr_accesses)
                     snapshot.regions.append(region)
-                result.snapshots.append(snapshot)
+                target_snapshots.append(snapshot)
     return result
 
 def perf_script_to_damon_result(perf_script_file):
@@ -91,20 +100,29 @@ def perf_script_to_damon_result(perf_script_file):
             continue
         if fields[4] != 'damon:damon_aggregated:':
             continue
-        time = int(float(fields[3][:-1]) * 1000000)
+        end_time = int(float(fields[3][:-1]) * 1000000)
         if not result:
-            result = DAMONResult(time)
+            result = DAMONResult()
 
         target_id = int(fields[5].split('=')[1])
+
+        if not target_id in result.snapshots:
+            result.snapshots[target_id] = []
+        target_snapshots = result.snapshots[target_id]
+        if len(target_snapshots) == 0:
+            start_time = -1
+        else:
+            start_time = target_snapshots[-1].end_time
+
         nr_regions = int(fields[6].split('=')[1])
         addrs = [int(x) for x in fields[7][:-1].split('-')]
         nr_accesses = int(fields[8])
 
         if nr_read_regions == 0:
-            snapshot = DAMONSnapshot(time, target_id)
-            result.snapshots.append(snapshot)
+            snapshot = DAMONSnapshot(start_time, end_time, target_id)
+            target_snapshots.append(snapshot)
 
-        snapshot = result.snapshots[-1]
+        snapshot = target_snapshots[-1]
         snapshot.regions.append(DAMONRegion(addrs[0], addrs[1], nr_accesses))
 
         nr_read_regions += 1
