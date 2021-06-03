31f5b0558d08f8cd0bd028f653e451525c5cca35
SeongJae Park <sjpark@amazon.de>
2021-06-03 14:27:43 +0000

    tests: Move report tests to separate directory
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- tests/report/test.sh
+++ tests/report/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+damo="../../damo"
+
 BINDIR=$(dirname "$0")
 cd "$BINDIR" || exit 1
 
@@ -21,26 +23,26 @@ test_report() {
 
 mkdir -p results
 
-test_report "../damo report raw" "raw"
+test_report "$damo report raw" "raw"
 
 test_report \
-	"../damo report raw --input_type perf_script -i perf.data.script" \
+	"$damo report raw --input_type perf_script -i perf.data.script" \
 	"raw_perf_script"
 
-test_report "../damo report wss -r 1 101 1 --raw_number" "wss"
+test_report "$damo report wss -r 1 101 1 --raw_number" "wss"
 
-test_report "../damo report wss -r 1 101 1 --work_time 1000000 --raw_number" \
+test_report "$damo report wss -r 1 101 1 --work_time 1000000 --raw_number" \
 	"wss_worktime_1s"
 
 test_report \
-	"../damo adjust 1000000 && ../damo report raw -i damon.adjusted.data" \
+	"$damo adjust 1000000 && $damo report raw -i damon.adjusted.data" \
 	"aggr_1s_raw"
 
-test_report "../damo report nr_regions -r 1 101 1" "nr_regions"
+test_report "$damo report nr_regions -r 1 101 1" "nr_regions"
 
-test_report "../damo report heats --guide" "heats_guide"
+test_report "$damo report heats --guide" "heats_guide"
 
-test_report "../damo report heats" "heats"
+test_report "$damo report heats" "heats"
 
 if perf script -l | grep -q damon
 then
