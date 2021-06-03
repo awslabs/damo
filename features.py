37818c9d0e82739733d30908b37201b98c4484e3
SeongJae Park <sjpark@amazon.de>
2021-06-03 13:53:47 +0000

    Add 'features' subcommand
    This commit adds 'features' subcommand, which let user know which DAMON
    features are supported in current kernel.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

---
+++ features.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import _damon
+
+def set_argparser(parser):
+    parser.add_argument('type', choices=['supported', 'unsupported', 'all'],
+            default='all',
+            help='type of features to listed')
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    features = _damon.get_supported_features()
+
+    for feature in features:
+        if args.type == 'all':
+            print('%s: %s' % (feature,
+                'Supported' if features[feature] else 'Unsupported'))
+        elif args.type == 'supported' and features[feature]:
+            print(feature)
+        elif args.type == 'unsupported' and not features[feature]:
+            print(feature)
+
+if __name__ == '__main__':
+    main()
