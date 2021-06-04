a310278998a228d75a6905331e140abe502c7d23
SeongJae Park <sjpark@amazon.de>
2021-06-04 11:58:36 +0000

    tests/schemes/test: Modularize
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- tests/schemes/test.sh
+++ tests/schemes/test.sh
@@ -6,20 +6,26 @@ cd "$bindir"
 damon_debugfs="/sys/kernel/debug/damon"
 damo="../../damo"
 
-python ./stairs.py &
-stairs_pid=$!
-sudo "$damo" schemes -c ./stat_cold_memory.damos "$stairs_pid" &
+test_stat() {
+	python ./stairs.py &
+	stairs_pid=$!
+	sudo "$damo" schemes -c ./stat_cold_memory.damos "$stairs_pid" &
 
-total_applied=0
-while ps --pid "$stairs_pid" > /dev/null
-do
-	applied=$(sudo cat "$damon_debugfs"/schemes | awk '{print $NF}')
-	sleep 2
-done
+	total_applied=0
+	while ps --pid "$stairs_pid" > /dev/null
+	do
+		applied=$(sudo cat "$damon_debugfs"/schemes | awk '{print $NF}')
+		sleep 2
+	done
 
-if [ "$applied" -eq 0 ]
-then
-	echo "FAIL schemes-stat"
-	exit 1
-fi
-echo "PASS schemes-stat ($applied cold memory found)"
+	if [ "$applied" -eq 0 ]
+	then
+		echo "FAIL schemes-stat"
+		exit 1
+	fi
+	echo "PASS schemes-stat ($applied cold memory found)"
+}
+
+test_stat
+
+echo "PASS $(basename $(pwd))"
