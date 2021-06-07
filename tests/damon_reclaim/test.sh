ba2e6241c0750352eba9dcc10d4bf397889d8533
SeongJae Park <sjpark@amazon.de>
2021-06-07 13:39:00 +0000

    tests/damon_reclaim: Add more test cases
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- tests/damon_reclaim/test.sh
+++ tests/damon_reclaim/test.sh
@@ -13,17 +13,53 @@ then
 	exit
 fi
 
+if [ ! -f "$sysdir/kdamond_pid" ]
+then
+	echo "SKIP $testname (kdamond_pid file not found)"
+	exit
+fi
+
 enabled=$(sudo cat "$sysdir/enabled")
-if [ "$enabled" = "N" ] && pgrep kdamond > /dev/null
+kdamond_pid=$(sudo cat "$sysdir/kdamond_pid")
+if [ "$enabled" = "N" ] && [ "$kdamond_pid" -ne -1 ]
 then
 	echo "FAIL damon_reclaim (disabled but kdamond running)"
 	exit 1
 fi
 
-if [ "$enabled" = "Y" ] && ! pgrep kdamond > /dev/null
+if [ "$enabled" = "Y" ] && [ "$kdamond_pid" -eq -1 ]
 then
 	echo "FAIL damon_reclaim (enabled but kdamond not running)"
 	exit 1
 fi
+
+echo N | sudo tee "$sysdir/enabled" > /dev/null && sleep 4
+if [ "$(sudo cat "$sysdir/kdamond_pid")" -ne -1 ]
+then
+	echo "FAIL damon_reclaim (disabling failed)"
+	exit 1
+fi
+
+echo Y | sudo tee "$sysdir/enabled" > /dev/null && sleep 2
+kdamond_pid=$(sudo cat "$sysdir/kdamond_pid")
+if [ "$kdamond_pid" -eq -1 ]
+then
+	echo "FAIL damon_reclaim (enabling failed)"
+	exit 1
+fi
+if ! ps --pid  "$kdamond_pid" > /dev/null
+then
+	echo "FAIL damon_reclaim (kdamond not started)"
+	exit 1
+fi
+
+echo N | sudo tee "$sysdir/enabled" > /dev/null && sleep 4
+kdamond_pid=$(sudo cat "$sysdir/kdamond_pid")
+if [ "$kdamond_pid" -ne -1 ]
+then
+	echo "FAIL damon_reclaim (disabling again failed: $kdamond_pid)"
+	exit 1
+fi
+
 
 echo "PASS $testname"
