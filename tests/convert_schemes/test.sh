c564720243090c260feb3e98393a60e21f95156f
SeongJae Park <sj38.park@gmail.com>
2021-08-07 11:40:29 +0200

    tests/convert_schemes: Test for all target scheme versions
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/convert_schemes/test.sh
+++ tests/convert_schemes/test.sh
@@ -9,15 +9,19 @@ damo="../../damo"
 test_convert() {
 	for input in ./inputs/*
 	do
-		scheme_name=$(basename "$input")
+		for version in {0..4}
+		do
+			scheme_name=$(basename "$input")
 
-		expected=$(cat "./expects/$scheme_name")
-		converted=$(../../_convert_damos.py "$input")
-		if [ ! "$expected" = "$converted" ]
-		then
-			echo "FAIL convert-schemes (for $input)"
-			exit 1
-		fi
+			expected=$(cat "./expects/$scheme_name.v$version")
+			converted=$(../../_convert_damos.py \
+				--scheme_version "$version" "$input")
+			if [ ! "$expected" = "$converted" ]
+			then
+				echo "FAIL convert-schemes (for $input)"
+				exit 1
+			fi
+		done
 	done
 }
 
