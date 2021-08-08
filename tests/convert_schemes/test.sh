a15d4c87600a5d773f8361645e44692d7d7f9956
SeongJae Park <sj38.park@gmail.com>
2021-08-08 11:53:54 +0200

    tests/convert_schemes: Test schemes text input case
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/convert_schemes/test.sh
+++ tests/convert_schemes/test.sh
@@ -6,7 +6,7 @@ cd "$bindir"
 damon_debugfs="/sys/kernel/debug/damon"
 damo="../../damo"
 
-test_convert() {
+test_convert_file() {
 	for input in ./inputs/*
 	do
 		for version in {0..4}
@@ -23,8 +23,31 @@ test_convert() {
 			fi
 		done
 	done
+	echo "PASS convert_schemes-file"
 }
 
-test_convert
+test_convert_txt() {
+	for input_file in ./inputs/*
+	do
+		for version in {0..4}
+		do
+			scheme_name=$(basename "$input_file")
+			expected=$(cat "./expects/$scheme_name.v$version")
+
+			input_txt=$(cat "$input_file")
+			converted=$(../../_convert_damos.py \
+				--scheme_version "$version" "$input_txt")
+			if [ ! "$expected" = "$converted" ]
+			then
+				echo "FAIL convert-schemes (for $input_file)"
+				exit 1
+			fi
+		done
+	done
+	echo "PASS convert_schemes-txt"
+}
+
+test_convert_file
+test_convert_txt
 
 echo "PASS $(basename $(pwd))"
