4e119a53b83bbabb848693be9c02ab83674b4341
SeongJae Park <sj38.park@gmail.com>
2021-08-08 11:31:32 +0200

    _convert_damos: Separate file operation
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _convert_damos.py
+++ _convert_damos.py
@@ -187,18 +187,23 @@ def debugfs_scheme(line, sample_interval, aggr_interval, scheme_version):
         print('Unsupported scheme version: %d' % scheme_version)
         exit(1)
 
+def convert_txt(schemes_txt, sample_interval, aggr_interval, scheme_version):
+    raw_lines = schemes_txt.split('\n')
+    converted_lines = []
+    for line in raw_lines:
+        if line.startswith('#'):
+            continue
+        line = line.strip()
+        if line == '':
+            continue
+        converted_lines.append(debugfs_scheme(line, sample_interval,
+            aggr_interval, scheme_version))
+    return '\n'.join(converted_lines)
+
 def convert(schemes_file, sample_interval, aggr_interval, scheme_version):
-    lines = []
     with open(schemes_file, 'r') as f:
-        for line in f:
-            if line.startswith('#'):
-                continue
-            line = line.strip()
-            if line == '':
-                continue
-            lines.append(debugfs_scheme(line, sample_interval, aggr_interval,
-                scheme_version))
-    return '\n'.join(lines)
+        return convert_txt(f.read(), sample_interval, aggr_interval,
+                scheme_version)
 
 def main():
     parser = argparse.ArgumentParser()
