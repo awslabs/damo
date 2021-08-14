f6f800106edb713c92f38b2cb79d919234f6145e
SeongJae Park <sj38.park@gmail.com>
2021-08-14 10:19:43 +0200

    adjust: Separate main logic to a function
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- adjust.py
+++ adjust.py
@@ -8,6 +8,35 @@ import struct
 
 import _damon_result
 
+def adjust_result(result, aggregate_interval, nr_snapshots_to_skip): 
+    snapshot_time = float(result.end_time - result.start_time) / result.nr_snapshots
+    nr_shots_in_aggr = int(max(round(aggregate_interval * 1000 /
+        snapshot_time), 1))
+    target_snapshots = result.target_snapshots
+
+    start_time = 0
+    end_time = 0
+    nr_snapshots = int(max((result.nr_snapshots - nr_snapshots_to_skip), 0) /
+            nr_shots_in_aggr)
+
+    for tid in target_snapshots:
+        # Skip first several snapshots as regions may not adjusted yet.
+        snapshots = target_snapshots[tid][nr_snapshots_to_skip:]
+        if start_time == 0:
+            start_time = snapshots[0].start_time
+            end_time = snapshots[-1].end_time
+
+        aggregated_snapshots = []
+        for i in range(0, len(snapshots), nr_shots_in_aggr):
+            to_aggregate = snapshots[i:
+                    min(i + nr_shots_in_aggr, len(snapshots))]
+            aggregated_snapshots.append(
+                    _damon_result.aggregate_snapshots(to_aggregate))
+        target_snapshots[tid] = aggregated_snapshots
+
+    result.start_time = start_time
+    result.end_time = end_time
+    result.nr_snapshots = nr_snapshots
 
 def set_argparser(parser):
     parser.add_argument('aggregate_interval', type=int,
@@ -32,35 +61,7 @@ def main(args=None):
         print('monitoring result file (%s) parsing failed' % file_path)
         exit(1)
 
-    snapshot_time = float(result.end_time - result.start_time) / result.nr_snapshots
-    nr_shots_in_aggr = int(max(round(args.aggregate_interval * 1000 /
-        snapshot_time), 1))
-    target_snapshots = result.target_snapshots
-
-    start_time = 0
-    end_time = 0
-    nr_snapshots = int(max((result.nr_snapshots - args.skip), 0) /
-            nr_shots_in_aggr)
-
-    for tid in target_snapshots:
-        # Skip first args.skip snapshots as regions may not adjusted yet.
-        snapshots = target_snapshots[tid][args.skip:]
-        if start_time == 0:
-            start_time = snapshots[0].start_time
-            end_time = snapshots[-1].end_time
-
-        aggregated_snapshots = []
-        for i in range(0, len(snapshots), nr_shots_in_aggr):
-            to_aggregate = snapshots[i:
-                    min(i + nr_shots_in_aggr, len(snapshots))]
-            aggregated_snapshots.append(
-                    _damon_result.aggregate_snapshots(to_aggregate))
-        target_snapshots[tid] = aggregated_snapshots
-
-    result.start_time = start_time
-    result.end_time = end_time
-    result.nr_snapshots = nr_snapshots
-
+    adjust_result(result, args.aggregate_interval, args.skip)
     _damon_result.write_damon_record(result, args.output, 2)
 
 if __name__ == '__main__':
