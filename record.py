awslabs/damo

commit 2773e71a69c6ca3f447e7903a77412a3ed92df90

record: Remove unused variable
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

record.py
@@ -141,7 +141,6 @@ def main(args=None):
     signal.signal(signal.SIGTERM, sighandler)
     orig_attrs = _damon.current_attrs()
 
-    args.schemes = ''
     pidfd = args.pidfd
     new_attrs = _damon.cmd_args_to_attrs(args)
     init_regions = _damon.cmd_args_to_init_regions(args)

commit d317c4a83ffed603bf01b0422353f57e12397a43

record: Restore 'args.schemes' setting for 'cmd_args_to_attrs()'
Commit 2773e71a69c6 ("record: Remove unused variable") removed
'args.schemes' setting, as it looked unnecessary.  However, it is
required by following '_damon.cmd_args_to_attrs()' call.  This commit
restores it for the reason.

Fixes: 2773e71a69c6 ("record: Remove unused variable")
Signed-off-by: SeongJae Park <sjpark@amazon.de>

record.py
@@ -137,6 +137,8 @@ def main(args=None):
     orig_attrs = _damon.current_attrs()
 
     pidfd = args.pidfd
+    # we don't use this, but _damon.cmd_args_to_attrs() require this.
+    args.schemes = ''
     new_attrs = _damon.cmd_args_to_attrs(args)
     init_regions = _damon.cmd_args_to_init_regions(args)
     numa_node = args.numa_node