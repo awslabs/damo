3cd22f5d74d6d98774ea21c5e03a89f29431f710
SeongJae Park <sjpark@amazon.de>
2021-09-06 08:33:40 +0000

    packaging/setup: Increase the version
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- packaging/setup.py
+++ packaging/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="damo",
-    version="0.0.1",
+    version="0.0.2",
     author="SeongJae Park",
     author_email="sjpark@amazon.de",
     description="DAMON user-space tool",
