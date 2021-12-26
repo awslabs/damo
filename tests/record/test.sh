e785d210231eaf84b398f14420900cd669c763f0
SeongJae Park <sj38.park@gmail.com>
2021-12-26 09:43:07 +0100

    tests/record: Use separate function for the body of the test
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/record/test.sh
+++ tests/record/test.sh
@@ -7,18 +7,23 @@ cd "$bindir"
 damon_debugfs="/sys/kernel/debug/damon"
 damo="../../damo"
 
-if ! sudo "$damo" record "sleep 3"
-then
-	echo "FAIL record-sleep-3: command failed"
-	exit 1
-fi
+test_record_validate()
+{
+	if ! sudo "$damo" record "sleep 3"
+	then
+		echo "FAIL record-validate-3: command failed"
+		exit 1
+	fi
 
-if ! "$damo" validate
-then
-	echo "FAIL record-validate-sleep-3"
-	exit 1
-fi
+	if ! "$damo" validate
+	then
+		echo "FAIL record-validate-sleep-3"
+		exit 1
+	fi
 
-echo "PASS record-validate-sleep-3"
+	echo "PASS record-validate-sleep-3"
+}
+
+test_record_validate
 
 echo "PASS $(basename $(pwd))"
