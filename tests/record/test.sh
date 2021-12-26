51687ea9dc5480deca8edc1b0a6fce4a4b10b21c
SeongJae Park <sj38.park@gmail.com>
2021-12-26 09:58:21 +0100

    tests/record: Cleanup files that generated
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/record/test.sh
+++ tests/record/test.sh
@@ -7,6 +7,22 @@ cd "$bindir"
 damon_debugfs="/sys/kernel/debug/damon"
 damo="../../damo"
 
+cleanup_files()
+{
+	files_to_remove="./damon.data ./damon.data.perf.data"
+	for file in $files_to_remove
+	do
+		if [ -f "$file" ]
+		then
+			if ! rm "$file"
+			then
+				echo "removing $file failed"
+				exit 1
+			fi
+		fi
+	done
+}
+
 test_leave_perf_data()
 {
 	if ! sudo "$damo" record "sleep 2" --leave_perf_data
@@ -20,6 +36,8 @@ test_leave_perf_data()
 		echo "FAIL record-leave-perf-data (perf.data not found)"
 		exit 1
 	fi
+
+	cleanup_files
 
 	echo "PASS record-leave-perf-data"
 }
@@ -43,6 +61,8 @@ test_record_validate()
 		echo "FAIL record-validate (perf.data is not removed)"
 		exit 1
 	fi
+
+	cleanup_files
 
 	echo "PASS record-validate-sleep-3"
 }
