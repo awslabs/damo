fc16bfe580784a15188ce902eb49846e431898ec
SeongJae Park <sj38.park@gmail.com>
2022-02-12 11:36:05 +0100

    _damon_sysfs: Implement 'set_target()'
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damon_sysfs.py
+++ _damon_sysfs.py
@@ -8,10 +8,34 @@ Contains core functions for DAMON sysfs control.
 import _damon
 
 feature_supports = None
-sysfs_damon = '/sys/kernel/mm/damon/admin/'
+
+def _write(content, filepath):
+    with open(filepath, 'w') as f:
+        f.write(content)
+
+def _ensure_sysfs_dir_for_damo():
+    if not os.isdir(sysfs_damon + 'kdamonds/0')):
+        _write('1', sysfs_damon + 'kdamonds/nr')
+    if not os.isdir(sysfs_damon + 'kdamonds/0/contexts/0'):
+        _write('1', sysfs_damon + 'kdamonds/0/contexts/nr')
 
 def set_target(tid, init_regions):
-    pass
+    _ensure_sysfs_dir_for_damo()
+    if not os.isdir(sysfs_damon + 'kdamonds/0/contexts/0/targets/0'):
+        _write('1', sysfs_damon + 'kdamonds/0/contexts/0/targets/nr')
+    if tid == 'paddr':
+        _write('paddr\n', sysfs_damon + 'kdamonds/0/contexts/0/damon_type')
+    else:
+        _write('%s\n' % tid, sysfs_damon +
+                'kdamonds/0/contexts/0/targets/0/pid')
+
+    _write('%s' % len(init_regions), sysfs_damon +
+            'kdamonds/0/contexts/0/targets/0/regions/nr')
+    for idx, region in enumerate(init_regions):
+        _write(region[0], sysfs_damon +
+                'kdamonds/0/contexts/0/targets/0/regions/%d/start')
+        _write(region[1], sysfs_damon +
+                'kdamonds/0/contexts/0/targets/0/regions/%d/end')
 
 def turn_damon(on_off):
     pass
