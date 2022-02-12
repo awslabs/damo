9c96f69358ee3cd02de1698a2fa2eed1fead735a
SeongJae Park <sj38.park@gmail.com>
2022-02-12 10:37:36 +0100

    Add DAMON sysfs interface control module
    This commit adds a module for control of DAMON sysfs interface.  It's
    not fully implemented at the moment.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ _damon_sysfs.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Contains core functions for DAMON sysfs control.
+"""
+
+def set_target(tid, init_regions):
+    pass
+
+def turn_damon(on_off):
+    pass
+
+def is_damon_running():
+    pass
+
+def attrs_apply(attrs)
+
+def current_attrs():
+    pass
+
+def feature_supported(feature):
+    pass
+
+def get_supported_features():
+    pass
+
+def chk_update(debugfs):
+    pass
+
+def cmd_args_to_attrs(args):
+    pass
+
+def cmd_args_to_init_regions(args):
+    pass
+
+def set_attrs_argparser(parser):
+    pass
+
+def set_init_regions_argparser(parser):
+    pass
