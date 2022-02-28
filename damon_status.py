b3450ebe7f9f895e327b19d48e8b50e485ce2527
SeongJae Park <sj38.park@gmail.com>
2022-02-28 09:58:13 +0100

    Add a new subcommand for showing DAMON status
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ damon_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import json
+
+import _damon
+
+def set_argparser(parser):
+    _damon.set_common_argparser(parser)
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    print(json.dumps(_damon.read_damon_fs(), indent=4, sort_keys=True))
+
+if __name__ == '__main__':
+    main()
