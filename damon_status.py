566a5c54ec28518c1966de0d0c5a038099e855ae
SeongJae Park <sj38.park@gmail.com>
2022-02-28 09:44:07 +0000

    status: Implement schemes_stats status show
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damon_status.py
+++ damon_status.py
@@ -24,6 +24,28 @@ def main(args=None):
     content = _damon.read_damon_fs()
     if args.target == 'all':
         print(json.dumps(content, indent=4, sort_keys=True))
+    elif args.target == 'schemes_stats':
+        kdamonds = content['kdamonds']
+        nr_kdamonds = int(kdamonds['nr_kdamonds'])
+        if nr_kdamonds == 0:
+            print('no kdamond exist')
+        for i in range(nr_kdamonds):
+            contexts = kdamonds['%d' % i]['contexts']
+            nr_contexts = int(contexts['nr_contexts'])
+            if nr_contexts == 0:
+                print('kdamond %d has no context' % i)
+                continue
+            for c in range(nr_contexts):
+                schemes = contexts['%d' % c]['schemes']
+                nr_schemes = int(schemes['nr_schemes'])
+                if nr_schemes == 0:
+                    print('kdamond %d context %d has no scheme' % (i, c))
+                    continue
+                for s in range(nr_schemes):
+                    stats = schemes['%d' % s]['stats']
+                    for f in ['nr_tried', 'sz_tried',
+                            'nr_applied', 'sz_applied', 'qt_exceeds']:
+                        print('%d %d %d %s: %d' % (i, c, s, f, int(stats[f])))
 
 if __name__ == '__main__':
     main()
