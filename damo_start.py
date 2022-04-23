d2f5672f1a5047b3d4879b553d7ef602ea372e65
SeongJae Park <sj38.park@gmail.com>
2022-04-23 09:38:13 +0000

    start: Add some more arguments
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_start.py
+++ damo_start.py
@@ -11,8 +11,12 @@ import _damon
 
 def set_argparser(parser):
     _damon.set_monitoring_argparser(parser)
+    parser.add_argument('--ops', choices=['vaddr', 'paddr', 'fvaddr'],
+            default='vaddr',
+            help='monitoring operations set')
+    parser.add_argument('--target_pid', type=int, help='target pid')
 
-def amin(args=None):
+def main(args=None):
     if not args:
         parser = argparse.ArgumentParser()
         set_argparser(parser)
