1f3f9cc9451d9a4842a51abcebba8be25982dfae
SeongJae Park <sj38.park@gmail.com>
2022-04-25 15:55:32 +0200

    damo_stop: Do initialization of underlying interface
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_stop.py
+++ damo_stop.py
@@ -10,6 +10,7 @@ import argparse
 import _damon
 
 def set_argparser(parser):
+    _damon.set_common_argparser(parser)
     return
 
 def main(args=None):
@@ -19,6 +20,7 @@ def main(args=None):
         args = parser.parse_args()
 
     _damon.ensure_root_permission()
+    err = _damon.initialize(args, skip_dirs_population=True)
 
     if not _damon.is_damon_running():
         print('DAMON is not turned on')
