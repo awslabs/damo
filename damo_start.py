dcbf1db0d8e1e71b08c03caf8b03fb792e931ca1
SeongJae Park <sj38.park@gmail.com>
2022-04-25 15:53:07 +0200

    damo_start: Make 'ops' positional argument
    The ops argument should be given everytime, so make it not optional but
    positional.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_start.py
+++ damo_start.py
@@ -11,7 +11,7 @@ import _damon
 
 def set_argparser(parser):
     _damon.set_monitoring_argparser(parser)
-    parser.add_argument('--ops', choices=['vaddr', 'paddr', 'fvaddr'],
+    parser.add_argument('ops', choices=['vaddr', 'paddr', 'fvaddr'],
             default='vaddr',
             help='monitoring operations set')
     parser.add_argument('--target_pid', type=int, help='target pid')
