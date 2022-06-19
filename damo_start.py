0771bfeffd8cd9d598fcee4d0302647d05a85cc3
SeongJae Park <sj38.park@gmail.com>
2022-06-19 20:09:36 +0000

    start: Use the new class based interface
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_start.py
+++ damo_start.py
@@ -25,32 +25,9 @@ def main(args=None):
         print(err)
         exit(1)
 
-    # TODO: Remove rbuf and out from the arguments in this case
-    args.rbuf = 0
-    args.out = 'null'
-    attrs = _damon.cmd_args_to_attrs(args)
-    init_regions = _damon.cmd_args_to_init_regions(args)
-    numa_node = args.numa_node
-
-    if args.ops == 'paddr':
-        target = 'paddr'
-        if not init_regions:
-            if numa_node != None:
-                init_regions = _damo_paddr_layout.paddr_region_of(numa_node)
-            else:
-                init_regions = [_damo_paddr_layout.default_paddr_region()]
-    elif target_has_pid(args.ops):
-        try:
-            pid = int(args.target_pid)
-        except:
-            print('target_pid (%s) is not an integer' % args.target_pid)
-            exit(1)
-        target = args.target_pid
-
-    if attrs.apply():
-        print('attributes (%s) failed to be applied' % attrs)
-    if _damon.set_target(target, init_regions):
-        print('target setting (%s, %s) failed' % (target, init_regions))
+    ctx = _damon.damon_ctx_from_damon_args(args)
+    kdamonds = [_damon.Kdamond('0', [ctx])]
+    _damon.apply_kdamonds(kdamonds)
     if _damon.turn_damon('on'):
         print('could not turn on damon')
     while not _damon.is_damon_running():
