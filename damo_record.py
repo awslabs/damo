229e31519d1fac60bd95be8ef47c01858125a914
SeongJae Park <sj38.park@gmail.com>
2022-07-16 18:04:35 +0000

    damo_record: Consolidate global vairables to better-named class
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_record.py
+++ damo_record.py
@@ -16,54 +16,60 @@ import _damon_dbgfs
 import _damon_result
 import _damo_paddr_layout
 
-perf_pipe = None
-rfile_path = None
-rfile_format = None
-target_is_ongoing = False
+class DataForCleanup:
+    target_is_ongoing = False
+    orig_attrs = None
+    rfile_path = None
+    rfile_format = None
+    rfile_permission = None
+    remove_perf_data = False
+    perf_pipe = None
 
-remove_perf_data = False
-rfile_permission = None
-def cleanup_exit(orig_attrs, exit_code):
+data_for_cleanup = DataForCleanup()
+
+def cleanup_exit(exit_code):
     rfile_mid_format = 'record'
-    if perf_pipe:
-        perf_data = rfile_path + '.perf.data'
-        perf_pipe.send_signal(signal.SIGINT)
-        perf_pipe.wait()
+    if data_for_cleanup.perf_pipe:
+        perf_data = data_for_cleanup.rfile_path + '.perf.data'
+        data_for_cleanup.perf_pipe.send_signal(signal.SIGINT)
+        data_for_cleanup.perf_pipe.wait()
         subprocess.call('perf script -i \'%s\' > \'%s\'' %
-                (perf_data, rfile_path),
+                (perf_data, data_for_cleanup.rfile_path),
                 shell=True, executable='/bin/bash')
         rfile_mid_format = 'perf_script'
 
-        if remove_perf_data:
+        if data_for_cleanup.remove_perf_data:
             os.remove(perf_data)
 
-    if not target_is_ongoing and _damon.is_damon_running():
+    if not data_for_cleanup.target_is_ongoing and _damon.is_damon_running():
         if _damon.turn_damon('off'):
             print('failed to turn damon off!')
         while _damon.is_damon_running():
             time.sleep(1)
-    if not target_is_ongoing and orig_attrs:
+    if not data_for_cleanup.target_is_ongoing and data_for_cleanup.orig_attrs:
         if _damon.damon_interface() != 'debugfs':
             print('damo_record/cleanup_exit: ' +
                     'BUG: none-debugfs is in use but orig_attrs is not None')
-        _damon_dbgfs.apply_debugfs_inputs(orig_attrs)
+        _damon_dbgfs.apply_debugfs_inputs(data_for_cleanup.orig_attrs)
 
-    if rfile_format != None and rfile_mid_format != rfile_format:
-        rfile_path_mid = rfile_path + '.mid'
-        os.rename(rfile_path, rfile_path_mid)
+    if (data_for_cleanup.rfile_format != None and
+            rfile_mid_format != data_for_cleanup.rfile_format):
+        rfile_path_mid = data_for_cleanup.rfile_path + '.mid'
+        os.rename(data_for_cleanup.rfile_path, rfile_path_mid)
         result = _damon_result.parse_damon_result(rfile_path_mid,
                 rfile_mid_format)
-        _damon_result.write_damon_result(result, rfile_path, rfile_format,
-                rfile_permission)
+        _damon_result.write_damon_result(result, data_for_cleanup.rfile_path,
+                data_for_cleanup.rfile_format,
+                data_for_cleanup.rfile_permission)
         os.remove(rfile_path_mid)
 
-    os.chmod(rfile_path, rfile_permission)
+    os.chmod(data_for_cleanup.rfile_path, data_for_cleanup.rfile_permission)
 
     exit(exit_code)
 
 def sighandler(signum, frame):
     print('\nsignal %s received' % signum)
-    cleanup_exit(orig_attrs, signum)
+    cleanup_exit(signum)
 
 def set_argparser(parser):
     _damon.set_implicit_target_monitoring_argparser(parser)
@@ -79,13 +85,7 @@ def set_argparser(parser):
             help='permission of the output file')
 
 def main(args=None):
-    global orig_attrs
-    global rfile_format
-    global rfile_path
-    global rfile_permission
-    global remove_perf_data
-    global perf_pipe
-    global target_is_ongoing
+    global data_for_cleanup
 
     if not args:
         parser = argparse.ArgumentParser()
@@ -93,8 +93,8 @@ def main(args=None):
         args = parser.parse_args()
 
     _damon.ensure_root_permission()
-    target_is_ongoing = args.target == 'ongoing'
-    if target_is_ongoing:
+    data_for_cleanup.target_is_ongoing = args.target == 'ongoing'
+    if data_for_cleanup.target_is_ongoing:
         skip_dirs_population = True
     else:
         skip_dirs_population = False
@@ -117,22 +117,24 @@ def main(args=None):
     if not args.rbuf:
         args.rbuf = 1024 * 1024
 
-    rfile_format = args.output_type
-    remove_perf_data = not args.leave_perf_data
-    rfile_permission = int(args.output_permission, 8)
-    if rfile_permission < 0o0 or rfile_permission > 0o777:
-        print('wrong --output_permission (%s)' % rfile_permission)
+    data_for_cleanup.rfile_format = args.output_type
+    data_for_cleanup.remove_perf_data = not args.leave_perf_data
+    data_for_cleanup.rfile_permission = int(args.output_permission, 8)
+    if (data_for_cleanup.rfile_permission < 0o0 or
+            data_for_cleanup.rfile_permission > 0o777):
+        print('wrong --output_permission (%s)' %
+                data_for_cleanup.rfile_permission)
         exit(1)
 
     signal.signal(signal.SIGINT, sighandler)
     signal.signal(signal.SIGTERM, sighandler)
 
     if _damon.damon_interface() == 'debugfs':
-        orig_attrs = _damon_dbgfs.current_debugfs_inputs()
+        data_for_cleanup.orig_attrs = _damon_dbgfs.current_debugfs_inputs()
     else:
-        orig_attrs = None
+        data_for_cleanup.orig_attrs = None
 
-    if not target_is_ongoing:
+    if not data_for_cleanup.target_is_ongoing:
         _damon.set_implicit_target_args_explicit(args)
         ctx = _damon.damon_ctx_from_damon_args(args)
         if damon_record_supported:
@@ -140,21 +142,23 @@ def main(args=None):
         kdamonds = [_damon.Kdamond('0', [ctx])]
         _damon.apply_kdamonds(kdamonds)
 
-    rfile_path = args.out
-    if os.path.isfile(rfile_path):
-        os.rename(rfile_path, rfile_path + '.old')
+    data_for_cleanup.rfile_path = args.out
+    if os.path.isfile(data_for_cleanup.rfile_path):
+        os.rename(data_for_cleanup.rfile_path,
+                data_for_cleanup.rfile_path + '.old')
 
-    if not target_is_ongoing:
+    if not data_for_cleanup.target_is_ongoing:
         if _damon.turn_damon('on'):
             print('could not turn DAMON on')
-            cleanup_exit(orig_attrs, -2)
+            cleanup_exit(-2)
 
         while not _damon.is_damon_running():
             time.sleep(1)
 
     if not damon_record_supported:
-        perf_pipe = subprocess.Popen(['perf', 'record', '-a',
-            '-e', 'damon:damon_aggregated', '-o', rfile_path + '.perf.data'])
+        data_for_cleanup.perf_pipe = subprocess.Popen(['perf', 'record', '-a',
+            '-e', 'damon:damon_aggregated', '-o',
+            data_for_cleanup.rfile_path + '.perf.data'])
     print('Press Ctrl+C to stop')
 
     if args.self_started_target == True:
@@ -162,7 +166,7 @@ def main(args=None):
     while _damon.is_damon_running():
         time.sleep(1)
 
-    cleanup_exit(orig_attrs, 0)
+    cleanup_exit(0)
 
 if __name__ == '__main__':
     main()
