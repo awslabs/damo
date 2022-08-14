daaa8937d4575916bc5ec7245d78eb0343ff19bd
SeongJae Park <sj38.park@gmail.com>
2022-08-14 17:10:36 +0000

    tests: Add damo-{start,stop} tests
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ tests/start_stop/test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+bindir=$(dirname "$0")
+cd "$bindir"
+
+damo="../../damo"
+
+testname=$(basename $(pwd))
+
+damon_interfaces=""
+if [ -d "/sys/kernel/debug/damon" ]
+then
+	damon_interfaces+="debugfs "
+fi
+
+if [ -d "/sys/kernel/mm/damon" ]
+then
+	damon_interfaces+="sysfs "
+fi
+
+if [ "$damon_interfaces" = "" ]
+then
+	echo "SKIP $(basename $(pwd)) (DAMON interface not found)"
+	exit 0
+fi
+
+for damon_interface in $damon_interfaces
+do
+	testname2="$testname $damon_interface"
+	sudo "$damo" start paddr
+	if ! pidof kdamond.0 > /dev/null
+	then
+		echo "FAIL $testname2 (kdamond.0 pid not found after start)"
+		exit 1
+	fi
+
+	sudo "$damo" stop
+	if pidof kdamond.0 > /dev/null
+	then
+		echo "FAIL $testname2 (kdamond.0 pid found after stop)"
+		exit 1
+	fi
+done
+
+echo "PASS $(basename $(pwd))"
