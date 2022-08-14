02d8607cba15a6adcb9550929b5d60631f7bfb93
SeongJae Park <sj38.park@gmail.com>
2022-08-14 18:14:37 +0000

    tests/run: Add start_stop tests in the tests list
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,8 @@
 bindir=$(dirname "$0")
 cd "$bindir" || exit 1
 
-for test_dir in record report convert_schemes schemes damon_reclaim damon_lru_sort
+for test_dir in record report convert_schemes schemes damon_reclaim damon_lru_sort \
+	start_stop
 do
 	if ! "./$test_dir/test.sh"
 	then
