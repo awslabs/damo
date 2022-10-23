0775164706161034d24f4b0e0c02ccce1f74d7b0
SeongJae Park <sj38.park@gmail.com>
2022-10-23 21:43:58 +0000

    test_damo: Add another test case for schemes output to Damos
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- test_damo.py
+++ test_damo.py
@@ -4,10 +4,25 @@
 import json
 import unittest
 
+import _damon
 import _damon_dbgfs
 import _damon_sysfs
 
 class TestDamonDbgfs(unittest.TestCase):
+    def test_debugfs_output_to_damos(self):
+        damos = _damon_dbgfs.debugfs_output_to_damos("4096 18446744073709551615 0 0 10 42949 5 0 584792941 1000 0 0 0 0 0 0 0 0 0 0 0 0 0\n")
+        expected = _damon.Damos('0',
+                access_pattern=_damon.DamosAccessPattern(4096,
+                    18446744073709551615, 0, 0, 'sample_intervals', 10, 42949,
+                    'aggr_intervals'),
+                action='stat',
+                quotas=_damon.DamosQuota(time_ms=0, sz_bytes=584792941,
+                    reset_interval_ms=1000, weight_sz_permil=0,
+                    weight_nr_accesses_permil=0, weight_age_permil=0),
+                watermarks=_damon.DamosWatermarks(0,0,0,0,0))
+
+        self.assertEqual(damos, expected)
+
     def test_files_content_to_kdamonds(self):
         _damon_dbgfs.feature_supports = {'init_regions': True, 'schemes': True,
                 'schemes_stat_qt_exceed': True, 'init_regions_target_idx':
