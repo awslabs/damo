561865d5f70fcc398ec08a95f57d9b0d29971ef4
SeongJae Park <sj38.park@gmail.com>
2022-10-29 19:13:47 +0000

    _damon_dbgfs/files_content_to_kdamonds(): Link record request
    files_content_to_kdamonds() reads and creates record request, but
    doesn't link it in the DAMON context.  Do it.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damon_dbgfs.py
+++ _damon_dbgfs.py
@@ -291,6 +291,7 @@ def files_content_to_kdamonds(files_content):
         schemes.append(debugfs_output_to_damos(line, intervals))
 
     ctx = _damon.DamonCtx('0', intervals, nr_regions, ops, targets, schemes)
+    ctx.record_request = record_request
     return [_damon.Kdamond('0', [ctx])]
 
 def current_kdamonds():
