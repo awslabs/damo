afa31678f9408570786faeffdf0c22ee7411809d
SeongJae Park <sj38.park@gmail.com>
2022-11-06 18:40:25 +0000

    test_damo/test_damo_schemes_to_damos: Test human readable scheme json inputs
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- test_damo.py
+++ test_damo.py
@@ -82,6 +82,41 @@ class TestDamos(unittest.TestCase):
                     }
                 ]
                 ''',
+                # human redable
+                '''
+                [
+                    {
+                        # some comment
+                        "name": "0",
+                        "action": "stat",
+                        "access_pattern": {
+                            "min_sz_bytes": "min",
+                            "max_sz_bytes": "min",
+                            "min_nr_accesses": 0,
+                            "max_nr_accesses": 0,
+                            "nr_accesses_unit": "sample_intervals",
+                            "min_age": 0,
+                            "max_age": 0,
+                            "age_unit": "aggr_intervals"
+                        },
+                        "quotas": {
+                            "time_ms": "0s",
+                            "sz_bytes": "0B",
+                            "reset_interval_ms": "0us",
+                            "weight_sz_permil": 0,
+                            "weight_nr_accesses_permil": 0,
+                            "weight_age_permil": 0
+                        },
+                        "watermarks": {
+                            "metric": "none",
+                            "interval_us": "0us",
+                            "high_permil": 0,
+                            "mid_permil": 0,
+                            "low_permil": 0
+                        }
+                    }
+                ]
+                ''',
         ]
         for txt in inputs:
             damos_list = _damo_schemes_input.damo_schemes_to_damos(txt)
