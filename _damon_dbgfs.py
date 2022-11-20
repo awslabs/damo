702d6bdf357c81e939cc4798d00da1822e9a507c
SeongJae Park <sj38.park@gmail.com>
2022-11-20 20:02:24 +0000

    _damon_dbgfs/wops_for_kdamonds: Support 0 context and 0 kdamonds
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damon_dbgfs.py
+++ _damon_dbgfs.py
@@ -114,10 +114,11 @@ def wops_for_kdamonds(kdamonds):
     if len(kdamonds) > 1:
         print('Currently only <=one kdamond is supported')
         exit(1)
-    if len(kdamonds[0].contexts) > 1:
+    if len(kdamonds) == 1 and len(kdamonds[0].contexts) > 1:
         print('currently only <= one damon_ctx is supported')
         exit(1)
-    if len(kdamonds[0].contexts[0].targets) > 1:
+    if (len(kdamonds) == 1 and len(kdamonds[0].contexts) == 1 and
+            len(kdamonds[0].contexts[0].targets) > 1):
         print('currently only <= one target is supported')
         exit(1)
     ctx = kdamonds[0].contexts[0]
