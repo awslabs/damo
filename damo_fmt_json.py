e60a92f53405f30a4c79aa1efca3a77552ebed3c
SeongJae Park <sj38.park@gmail.com>
2022-12-03 19:58:13 +0000

    damo: Add a subcommand for json input formatting
    json format is the next standard, but hard to deal with from the
    scratch.  Add a new damo subcommand, 'fmt_json', which can be used for
    converting 'damo start' cmdline to json.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ damo_fmt_json.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Convert args to DAMON json input.
+"""
+
+import argparse
+import json
+
+import _damon_args
+
+def set_argparser(parser):
+    _damon_args.set_explicit_target_argparser(parser)
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    kdamonds = [k.to_kvpairs() for k in
+            _damon_args.kdamonds_from_damon_args(args)]
+    print(json.dumps(kdamonds, indent=4))
+
+if __name__ == '__main__':
+    main()
