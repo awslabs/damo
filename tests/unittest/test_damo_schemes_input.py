2a865902844ebce313eec55fe8fd1cf5382854ba
SeongJae Park <sj38.park@gmail.com>
2022-12-17 21:20:08 +0000

    tests/unittest/test_damo_schemes_input/test_damo_schemes_to_damos: Reduce code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unittest/test_damo_schemes_input.py
+++ tests/unittest/test_damo_schemes_input.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0
 
+import copy
 import json
 import os
 import sys
@@ -13,74 +14,73 @@ sys.path.append(damo_dir)
 import _damon
 import _damo_schemes_input
 
+base_damos_kv = [
+        {
+            "name": "0",
+            "action": "stat",
+            "access_pattern": {
+                "min_sz_bytes": 0,
+                "max_sz_bytes": 0,
+                "min_nr_accesses": "0 sample_intervals",
+                "max_nr_accesses": "0 sample_intervals",
+                "min_age": "0 aggr_intervals",
+                "max_age": "0 aggr_intervals"
+                },
+            "quotas": {
+                "time_ms": 0,
+                "sz_bytes": 0,
+                "reset_interval_ms": 0,
+                "weight_sz_permil": 0,
+                "weight_nr_accesses_permil": 0,
+                "weight_age_permil": 0
+                },
+            "watermarks": {
+                "metric": "none",
+                "interval_us": 0,
+                "high_permil": 0,
+                "mid_permil": 0,
+                "low_permil": 0
+                }
+            }
+        ]
+human_readable_damos_kv = [
+        {
+            "name": "0",
+            "action": "stat",
+            "access_pattern": {
+                "min_sz_bytes": "min",
+                "max_sz_bytes": "min",
+                "min_nr_accesses": "0 sample_intervals",
+                "max_nr_accesses": "0 sample_intervals",
+                "min_age": "0 aggr_intervals",
+                "max_age": "0 aggr_intervals"
+                },
+            "quotas": {
+                "time_ms": "0s",
+                "sz_bytes": "0B",
+                "reset_interval_ms": "0us",
+                "weight_sz_permil": 0,
+                "weight_nr_accesses_permil": 0,
+                "weight_age_permil": 0
+                },
+            "watermarks": {
+                "metric": "none",
+                "interval_us": "0us",
+                "high_permil": 0,
+                "mid_permil": 0,
+                "low_permil": 0
+                }
+            }
+        ]
+
 class TestDamoSchemesInput(unittest.TestCase):
     def test_damo_schemes_to_damos_without_filters(self):
-
-        base_damos_kv = [
-                {
-                    "name": "0",
-                    "action": "stat",
-                    "access_pattern": {
-                        "min_sz_bytes": 0,
-                        "max_sz_bytes": 0,
-                        "min_nr_accesses": "0 sample_intervals",
-                        "max_nr_accesses": "0 sample_intervals",
-                        "min_age": "0 aggr_intervals",
-                        "max_age": "0 aggr_intervals"
-                        },
-                    "quotas": {
-                        "time_ms": 0,
-                        "sz_bytes": 0,
-                        "reset_interval_ms": 0,
-                        "weight_sz_permil": 0,
-                        "weight_nr_accesses_permil": 0,
-                        "weight_age_permil": 0
-                        },
-                    "watermarks": {
-                        "metric": "none",
-                        "interval_us": 0,
-                        "high_permil": 0,
-                        "mid_permil": 0,
-                        "low_permil": 0
-                        }
-                    }
-                ]
-        human_readable_damos_kv = [
-                {
-                    "name": "0",
-                    "action": "stat",
-                    "access_pattern": {
-                        "min_sz_bytes": "min",
-                        "max_sz_bytes": "min",
-                        "min_nr_accesses": "0 sample_intervals",
-                        "max_nr_accesses": "0 sample_intervals",
-                        "min_age": "0 aggr_intervals",
-                        "max_age": "0 aggr_intervals"
-                        },
-                    "quotas": {
-                        "time_ms": "0s",
-                        "sz_bytes": "0B",
-                        "reset_interval_ms": "0us",
-                        "weight_sz_permil": 0,
-                        "weight_nr_accesses_permil": 0,
-                        "weight_age_permil": 0
-                        },
-                    "watermarks": {
-                        "metric": "none",
-                        "interval_us": "0us",
-                        "high_permil": 0,
-                        "mid_permil": 0,
-                        "low_permil": 0
-                        }
-                    }
-                ]
-
-base_damos_str = json.dumps(base_damos_kv, indent=4)
-base_damos_str_with_comments = '\n\n# some comments\n'.join(
-        base_damos_str.split('\n'))
-human_readable_damos_str = json.dumps(human_readable_damos_kv, indent=4)
-human_readable_damos_kv_with_comments = '\n\n# some comments\n'.join(
-        human_readable_damos_str.split('\n'))
+        base_damos_str = json.dumps(base_damos_kv, indent=4)
+        base_damos_str_with_comments = '\n\n# some comments\n'.join(
+                base_damos_str.split('\n'))
+        human_readable_damos_str = json.dumps(human_readable_damos_kv, indent=4)
+        human_readable_damos_kv_with_comments = '\n\n# some comments\n'.join(
+                human_readable_damos_str.split('\n'))
 
         inputs = [base_damos_str, base_damos_str_with_comments,
                 human_readable_damos_str,
@@ -96,148 +96,42 @@ human_readable_damos_kv_with_comments = '\n\n# some comments\n'.join(
             self.assertEqual(damos_list[0], expected)
 
     def test_damo_schemes_to_damos(self):
-        inputs = [
-                # no comment
-                '''
-                [
-                    {
-                        "name": "0",
-                        "action": "stat",
-                        "access_pattern": {
-                            "min_sz_bytes": 0,
-                            "max_sz_bytes": 0,
-                            "min_nr_accesses": "0 sample_intervals",
-                            "max_nr_accesses": "0 sample_intervals",
-                            "min_age": "0 aggr_intervals",
-                            "max_age": "0 aggr_intervals"
-                        },
-                        "quotas": {
-                            "time_ms": 0,
-                            "sz_bytes": 0,
-                            "reset_interval_ms": 0,
-                            "weight_sz_permil": 0,
-                            "weight_nr_accesses_permil": 0,
-                            "weight_age_permil": 0
-                        },
-                        "watermarks": {
-                            "metric": "none",
-                            "interval_us": 0,
-                            "high_permil": 0,
-                            "mid_permil": 0,
-                            "low_permil": 0
-                        },
-                        "filters": [
-                            {
-                                "name": "0",
-                                "filter_type": "anon",
-                                "memcg_path": "",
-                                "matching": "true"
-                             },
-                             {
-                                "name": "1",
-                                "filter_type": "memcg",
-                                "memcg_path": "/all/latency-critical",
-                                "matching": "false"
-                            }
-                        ]
+        base_damos_kv_with_filters = copy.deepcopy(base_damos_kv)
+        base_filters_kv = [
+                {
+                    "name": "0",
+                    "filter_type": "anon",
+                    "memcg_path": "",
+                    "matching": "true"
+                    },
+                {
+                    "name": "1",
+                    "filter_type": "memcg",
+                    "memcg_path": "/all/latency-critical",
+                    "matching": "false"
                     }
                 ]
-                ''',
-                # with comments
-                '''
-                [
-                    {
-                        # some comment
-                        "name": "0",
-                        "action": "stat",
-                        "access_pattern": {
-                            "min_sz_bytes": 0,
-                            "max_sz_bytes": 0,
-                            "min_nr_accesses": "0 sample_intervals",
-                            "max_nr_accesses": "0 sample_intervals",
-                            "min_age": "0 aggr_intervals",
-                            "max_age": "0 aggr_intervals"
-                        },
-                        "quotas": {
-                            "time_ms": 0,
-                            "sz_bytes": 0,
-                            "reset_interval_ms": 0,
-                            "weight_sz_permil": 0,
-                            "weight_nr_accesses_permil": 0,
-                            "weight_age_permil": 0
-                        },
-                        "watermarks": {
-                            "metric": "none",
-                            "interval_us": 0,
-                            "high_permil": 0,
-                            "mid_permil": 0,
-                            "low_permil": 0
-                        },
-                        "filters": [
-                            {
-                                "name": "0",
-                                "filter_type": "anon",
-                                "memcg_path": "",
-                                "matching": "true"
-                             },
-                             {
-                                "name": "1",
-                                "filter_type": "memcg",
-                                "memcg_path": "/all/latency-critical",
-                                "matching": "false"
-                            }
-                        ]
-                    }
-                ]
-                ''',
-                # human redable
-                '''
-                [
-                    {
-                        # some comment
-                        "name": "0",
-                        "action": "stat",
-                        "access_pattern": {
-                            "min_sz_bytes": "min",
-                            "max_sz_bytes": "min",
-                            "min_nr_accesses": "0 sample_intervals",
-                            "max_nr_accesses": "0 sample_intervals",
-                            "min_age": "0 aggr_intervals",
-                            "max_age": "0 aggr_intervals"
-                        },
-                        "quotas": {
-                            "time_ms": "0s",
-                            "sz_bytes": "0B",
-                            "reset_interval_ms": "0us",
-                            "weight_sz_permil": 0,
-                            "weight_nr_accesses_permil": 0,
-                            "weight_age_permil": 0
-                        },
-                        "watermarks": {
-                            "metric": "none",
-                            "interval_us": "0us",
-                            "high_permil": 0,
-                            "mid_permil": 0,
-                            "low_permil": 0
-                        },
-                        "filters": [
-                            {
-                                "name": "0",
-                                "filter_type": "anon",
-                                "memcg_path": "",
-                                "matching": "true"
-                             },
-                             {
-                                "name": "1",
-                                "filter_type": "memcg",
-                                "memcg_path": "/all/latency-critical",
-                                "matching": "false"
-                            }
-                        ]
-                    }
-                ]
-                ''',
-        ]
+        base_damos_kv_with_filters[0]['filters'] = base_filters_kv
+        human_readable_damos_kv_with_filters = copy.deepcopy(
+                human_readable_damos_kv)
+        human_readable_damos_kv_with_filters[0]['filters'] = base_filters_kv
+
+        base_damos_with_filters_str = json.dumps(base_damos_kv_with_filters,
+                indent=4)
+        base_damos_with_filters_str_with_comments = (
+                '\n\n# some comments\n'.join(
+                    base_damos_with_filters_str.split('\n')))
+        human_readable_damos_with_filters_str = json.dumps(
+                human_readable_damos_kv_with_filters, indent=4)
+        human_readable_damos_with_filters_str_with_comments = (
+                '\n\n# some comments\n'.join(
+                    human_readable_damos_with_filters_str.split('\n')))
+
+        inputs = [base_damos_with_filters_str,
+            base_damos_with_filters_str_with_comments,
+            human_readable_damos_with_filters_str,
+            human_readable_damos_with_filters_str_with_comments]
+
         for txt in inputs:
             damos_list = _damo_schemes_input.damo_schemes_to_damos(txt)
             expected = _damon.Damos('0',
