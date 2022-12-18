dd223833f43c6bce2eac385fd834e004aac9ef6d
SeongJae Park <sj38.park@gmail.com>
2022-12-18 20:12:31 +0000

    tests/unittest/test_damon_dbgfs: Cleanup code
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unittest/test_damon_dbgfs.py
+++ tests/unittest/test_damon_dbgfs.py
@@ -33,13 +33,15 @@ class TestDamonDbgfs(unittest.TestCase):
                     x, _damon.DamonIntervals(5000, 100000, 1000000)),
                 {"4096 18446744073709551615 0 0 10 42949 5 0 584792941 1000 0 0 0 0 0 0 0 0 0 0 0 0 0\n":
                     _damon.Damos('0',
-                        access_pattern=_damon.DamosAccessPattern(4096,
-                            18446744073709551615, 0.0, 0.0, 'percent', 1000000.0,
-                            4294900000.0, 'usec'),
+                        access_pattern=_damon.DamosAccessPattern(
+                            4096, 18446744073709551615,
+                            0.0, 0.0, 'percent',
+                            1000000.0, 4294900000.0, 'usec'),
                         action='stat',
-                        quotas=_damon.DamosQuotas(time_ms=0, sz_bytes=584792941,
-                            reset_interval_ms=1000, weight_sz_permil=0,
-                            weight_nr_accesses_permil=0, weight_age_permil=0),
+                        quotas=_damon.DamosQuotas(time_ms=0,
+                            sz_bytes=584792941, reset_interval_ms=1000,
+                            weight_sz_permil=0, weight_nr_accesses_permil=0,
+                            weight_age_permil=0),
                         watermarks=_damon.DamosWatermarks('none',0,0,0,0),
                         filters=[], stats=None)})
 
