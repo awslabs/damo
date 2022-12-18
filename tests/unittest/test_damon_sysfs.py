f59830fee5eb0be6f73f5dad5e5f0bb154273d92
SeongJae Park <sj38.park@gmail.com>
2022-12-18 20:58:25 +0000

    tests/unittest/test_damon_sysfs: Further cleanup
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unittest/test_damon_sysfs.py
+++ tests/unittest/test_damon_sysfs.py
@@ -30,42 +30,38 @@ class TestDamonSysfs(unittest.TestCase):
 
         sysfs_dict = {
                 "0": {
-                    "avail_state_inputs": "on\noff\ncommit\nupdate_schemes_stats\nupdate_schemes_tried_regions\nclear_schemes_tried_regions\n",
+                    "nr_kdamonds": "1\n",
+                    "state": "off\n",
+                    "pid": "-1\n",
+                    "avail_state_inputs": '\n'.join(['on', 'off', 'commit',
+                        'update_schemes_stats', 'update_schemes_tried_regions',
+                        'clear_schemes_tried_regions\n']),
                     "contexts": {
+                        "nr_contexts": "1\n",
                         "0": {
                             "avail_operations": "vaddr\nfvaddr\npaddr\n",
+                            "operations": "paddr\n",
                             "monitoring_attrs": {
                                 "intervals": {
-                                    "aggr_us": "200000\n",
                                     "sample_us": "5000\n",
-                                    "update_us": "1000000\n"
+                                    "update_us": "1000000\n",
+                                    "aggr_us": "200000\n",
                                     },
                                 "nr_regions": {
-                                    "max": "1000\n",
-                                    "min": "10\n"
-                                    }
+                                    "max": "1000\n", "min": "10\n"}
                                 },
-                            "operations": "paddr\n",
+                            "targets": {"nr_targets": "0\n"},
                             "schemes": {
+                                "nr_schemes": "1\n",
                                 "0": {
                                     "access_pattern": {
-                                        "age": {
-                                            "max": "0\n",
-                                            "min": "0\n"
-                                            },
-                                        "nr_accesses": {
-                                            "max": "0\n",
-                                            "min": "0\n"
-                                            },
-                                        "sz": {
-                                            "max": "0\n",
-                                            "min": "0\n"
-                                            }
-                                        },
+                                        "age": {"max": "0\n", "min": "0\n"},
+                                        "nr_accesses":
+                                        {"max": "0\n", "min": "0\n"},
+                                        "sz": {"max": "0\n", "min": "0\n"}},
                                     "action": "stat\n",
                                     "quotas": {
-                                        "bytes": "0\n",
-                                        "ms": "0\n",
+                                        "bytes": "0\n", "ms": "0\n",
                                         "reset_interval_ms": "0\n",
                                         "weights": {
                                             "age_permil": "0\n",
@@ -73,80 +69,52 @@ class TestDamonSysfs(unittest.TestCase):
                                             "sz_permil": "0\n"
                                             }
                                         },
+                                    "watermarks": {
+                                        "metric": "none\n", "high": "0\n",
+                                        "mid": "0\n", "low": "0\n",
+                                        "interval_us": "0\n",
+                                        },
                                     "stats": {
-                                        "nr_applied": "0\n",
-                                        "nr_tried": "0\n",
+                                        "nr_applied": "0\n", "nr_tried": "0\n",
+                                        "sz_applied": "0\n", "sz_tried": "0\n",
                                         "qt_exceeds": "0\n",
-                                        "sz_applied": "0\n",
-                                        "sz_tried": "0\n"
                                         },
                                     "tried_regions": {},
-                                    "watermarks": {
-                                        "high": "0\n",
-                                        "interval_us": "0\n",
-                                        "low": "0\n",
-                                        "metric": "none\n",
-                                        "mid": "0\n"
-                                        }
-                                    },
-                                "nr_schemes": "1\n"
+                                   },
                                 },
-                            "targets": {
-                                    "nr_targets": "0\n"
-                                    }
                             },
-                    "nr_contexts": "1\n"
-                },
-                "pid": "-1\n",
-                "state": "off\n"
-            },
-            "nr_kdamonds": "1\n"
-        }
+                        },
+                    }
+                }
 
         expected_wops = {
                 "0": {
                     "contexts": {
                         "0": [
-                            {
-                                "operations": "paddr"
-                                },
+                            {"operations": "paddr"},
                             {
                                 "monitoring_attrs": {
                                     "intervals": {
-                                        "aggr_us": "200000",
                                         "sample_us": "5000",
+                                        "aggr_us": "200000",
                                         "update_us": "1000000"
                                         },
-                                    "nr_regions": {
-                                        "max": "1000",
-                                        "min": "10"
-                                        }
+                                    "nr_regions": {"max": "1000", "min": "10"}
                                     }
                                 },
-                            {
-                                "targets": {}
-                                },
+                            {"targets": {}},
                             {
                                 "schemes": {
                                     "0": {
                                         "access_pattern": {
-                                            "age": {
-                                                "max": "0",
-                                                "min": "0"
-                                                },
+                                            "age": {"max": "0", "min": "0"},
                                             "nr_accesses": {
-                                                "max": "0",
-                                                "min": "0"
-                                                },
-                                            "sz": {
-                                                "max": "0",
-                                                "min": "0"
-                                                }
+                                                "max": "0", "min": "0"},
+                                            "sz": {"max": "0", "min": "0"}
                                             },
                                         "action": "stat",
                                         "quotas": {
-                                            "bytes": "0",
-                                            "ms": "0",
+                                            "bytes": "0", "ms": "0",
                                             "reset_interval_ms": "0",
                                             "weights": {
                                                 "age_permil": "0",
@@ -155,11 +123,9 @@ class TestDamonSysfs(unittest.TestCase):
                                                 }
                                             },
                                         "watermarks": {
-                                            "high": "0",
-                                            "interval_us": "0",
-                                            "low": "0",
                                             "metric": "none",
-                                            "mid": "0"
+                                            "interval_us": "0", "high": "0",
+                                            "mid": "0", "low": "0",
                                             },
                                         "filters": {
                                             }
