4dca5eb37cc3e3ac5901486ca17e86c9a6d6fe71
SeongJae Park <sj38.park@gmail.com>
2022-12-26 19:35:53 +0000

    damo_fmt_json: Support '--raw' option
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_fmt_json.py
+++ damo_fmt_json.py
@@ -12,6 +12,8 @@ import _damon_args
 
 def set_argparser(parser):
     _damon_args.set_explicit_target_argparser(parser)
+    parser.add_argument('--raw', action='store_true',
+            help='print numbers in machine friendly raw form')
 
 def main(args=None):
     if not args:
@@ -19,7 +21,7 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    kdamonds = [k.to_kvpairs() for k in
+    kdamonds = [k.to_kvpairs(args.raw) for k in
             _damon_args.kdamonds_from_damon_args(args)]
     print(json.dumps(kdamonds, indent=4))
 
