d9a97cf631f680c74c62331faf07403d493baa2b
SeongJae Park <sj38.park@gmail.com>
2022-12-31 18:49:41 +0000

    tests/unittest: Add a test for _damon_args
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ tests/unittest/test_damon_args.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import argparse
+import os
+import sys
+import unittest
+
+bindir = os.path.dirname(os.path.realpath(__file__))
+damo_dir = os.path.join(bindir, '..', '..')
+sys.path.append(damo_dir)
+
+import _damon
+import _damon_args
+
+class TestDamonArgs(unittest.TestCase):
+    def test_damon_ctx_from_damon_args(self):
+        self.assertEqual(_damon_args.damon_ctx_from_damon_args(
+            argparse.Namespace(**{'sample': '5000', 'aggr': '100000',
+                'updr': '1000000', 'minr': 10, 'maxr': 1000,
+                'regions': '123-456', 'numa_node': None, 'ops': 'paddr',
+                'target_pid': None, 'schemes': None})),
+            _damon.DamonCtx('0',
+                _damon.DamonIntervals(5000, 100000, 1000000),
+                _damon.DamonNrRegionsRange(10, 1000), 'paddr',
+                [_damon.DamonTarget('0', None,
+                    [_damon.DamonRegion(123, 456)])],
+                []))
+
+if __name__ == '__main__':
+    unittest.main()
