e2c0004a8a6d8a9a8ce7dcf32560c4ec8973704a
SeongJae Park <sj38.park@gmail.com>
2022-12-31 19:16:20 +0000

    tests/unittest/test_damon_args: Test human readsable regions
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unittest/test_damon_args.py
+++ tests/unittest/test_damon_args.py
@@ -30,13 +30,13 @@ class TestDamonArgs(unittest.TestCase):
         self.assertEqual(_damon_args.damon_ctx_from_damon_args(
             argparse.Namespace(**{'sample': '5ms', 'aggr': '100ms',
                 'updr': '1s', 'minr': '10', 'maxr': '1,000',
-                'regions': '123-456', 'numa_node': None, 'ops': 'paddr',
+                'regions': '1K-4K', 'numa_node': None, 'ops': 'paddr',
                 'target_pid': None, 'schemes': None})),
             _damon.DamonCtx('0',
                 _damon.DamonIntervals(5000, 100000, 1000000),
                 _damon.DamonNrRegionsRange(10, 1000), 'paddr',
                 [_damon.DamonTarget('0', None,
-                    [_damon.DamonRegion(123, 456)])],
+                    [_damon.DamonRegion(1024, 4096)])],
                 []))
 
 if __name__ == '__main__':
