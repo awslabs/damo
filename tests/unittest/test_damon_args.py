50cb1c85be5a4da4bae6fe2d82037d2b3ed077ff
SeongJae Park <sj38.park@gmail.com>
2022-12-31 18:54:29 +0000

    tests/unittest/test_damon_args: Test human readable intervals
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unittest/test_damon_args.py
+++ tests/unittest/test_damon_args.py
@@ -27,5 +27,17 @@ class TestDamonArgs(unittest.TestCase):
                     [_damon.DamonRegion(123, 456)])],
                 []))
 
+        self.assertEqual(_damon_args.damon_ctx_from_damon_args(
+            argparse.Namespace(**{'sample': '5ms', 'aggr': '100ms',
+                'updr': '1s', 'minr': 10, 'maxr': 1000,
+                'regions': '123-456', 'numa_node': None, 'ops': 'paddr',
+                'target_pid': None, 'schemes': None})),
+            _damon.DamonCtx('0',
+                _damon.DamonIntervals(5000, 100000, 1000000),
+                _damon.DamonNrRegionsRange(10, 1000), 'paddr',
+                [_damon.DamonTarget('0', None,
+                    [_damon.DamonRegion(123, 456)])],
+                []))
+
 if __name__ == '__main__':
     unittest.main()
