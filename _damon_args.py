awslabs/damo

commit eb496047e7b6dfd9a57846a329fca732636adcd7

_damon_args/damon_ctx_from_damon_args(): Handle record request
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_args.py
@@ -70,13 +70,20 @@ def damon_ctx_from_damon_args(args):
     except Exception as e:
         return 'Wrong \'--target_pid\' argument (%s)' % e
 
+    record_request = None
+    if _damon.feature_supported('record') and 'rbuf' in args:
+        record_request = _damon.DamonRecord(args.rbuf, args.out)
+
     schemes, err = damos_from_args(args)
     if err:
         return err
 
     try:
-        return _damon.DamonCtx(
-                '0', intervals, nr_regions, ops, [target], schemes), None
+        ctx = _damon.DamonCtx(
+                '0', intervals, nr_regions, ops, [target], schemes)
+        if record_request:
+            ctx.record_request = record_request
+        return ctx, None
     except Exception as e:
         return None, 'Creating context from arguments failed (%s)' % e
 
@@ -158,8 +165,6 @@ def turn_implicit_args_damon_on(args):
     ctx, err = damon_ctx_from_damon_args(args)
     if err:
         return err, None
-    if _damon.feature_supported('record') and 'rbuf' in args:
-        ctx.record_request = _damon.DamonRecord(args.rbuf, args.out)
     kdamonds = [_damon.Kdamond('0', state=None, pid=None, contexts=[ctx])]
     err = _damon.apply_kdamonds(kdamonds)
     if err:

commit 2ff5ff0603531f6a05511238d594a2006070204a

_damon_args: Handle record request regardless of the record feature
Commit eb496047e7b6 ("_damon_args/damon_ctx_from_damon_args(): Handle
record request") made damon_ctx_from_damon_args() depends on feature
check.  'damo fmt_json' fails for the reason.  Remove the feature check
to keep it feature independent.  The feature check will anyway done in
_damon_dbgfs when applying the input.

Fixes: eb496047e7b6 ("_damon_args/damon_ctx_from_damon_args(): Handle record request")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_args.py
@@ -76,7 +76,7 @@ def damon_ctx_from_damon_args(args):
         return 'Wrong \'--target_pid\' argument (%s)' % e
 
     record_request = None
-    if _damon.feature_supported('record') and 'rbuf' in args:
+    if 'rbuf' in args:
         try:
             record_request = _damon.DamonRecord(args.rbuf, args.out)
         except Exception as e: