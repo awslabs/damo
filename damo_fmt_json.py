awslabs/damo

commit e448af8f041de8ef10eef384d47053bdbad84d84

_damon_args: damon_ctx_from_damon_args(): Return error
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

damo_fmt_json.py
@@ -21,8 +21,11 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    kdamonds = [k.to_kvpairs(args.raw) for k in
+    kdamonds, err = [k.to_kvpairs(args.raw) for k in
             _damon_args.kdamonds_from_damon_args(args)]
+    if err:
+        print('creating kdamonds from arguments failed (%s)' % err)
+        exit(1)
     print(json.dumps(kdamonds, indent=4))
 
 if __name__ == '__main__':

_damon_args.py
@@ -22,8 +22,14 @@ def damos_from_args(args):
     return _damo_schemes_input.damo_schemes_to_damos(args.schemes)
 
 def damon_ctx_from_damon_args(args):
-    intervals = _damon.DamonIntervals(args.sample, args.aggr, args.updr)
-    nr_regions = _damon.DamonNrRegionsRange(args.minr, args.maxr)
+    try:
+        intervals = _damon.DamonIntervals(args.sample, args.aggr, args.updr)
+    except Exception as e:
+        return None, 'invalid intervals arguments (%s)' % e
+    try:
+        nr_regions = _damon.DamonNrRegionsRange(args.minr, args.maxr)
+    except Exception as e:
+        return None, 'invalid nr_regions arguments (%s)' % e
     ops = args.ops
 
     init_regions = []
@@ -39,8 +45,7 @@ def damon_ctx_from_damon_args(args):
                 if init_regions and init_regions[-1].end > region.start:
                     raise Exception('regions overlap')
             except Exception as e:
-                print('Wrong \'--regions\' argument (%s)' % e)
-                exit(1)
+                return None, 'Wrong \'--regions\' argument (%s)' % e
             init_regions.append(region)
 
     if ops == 'paddr' and not init_regions:
@@ -48,14 +53,25 @@ def damon_ctx_from_damon_args(args):
             init_regions = _damo_paddr_layout.paddr_region_of(args.numa_node)
         else:
             init_regions = [_damo_paddr_layout.default_paddr_region()]
-        init_regions = [_damon.DamonRegion(r[0], r[1]) for r in init_regions]
+        try:
+            init_regions = [_damon.DamonRegion(r[0], r[1])
+                    for r in init_regions]
+        except Exception as e:
+            return 'Wrong \'--regions\' argument (%s)' % e
 
-    target = _damon.DamonTarget('0', args.target_pid
-            if _damon.target_has_pid(ops) else None, init_regions)
+    try:
+        target = _damon.DamonTarget('0', args.target_pid
+                if _damon.target_has_pid(ops) else None, init_regions)
+    except Exception as e:
+        return 'Wrong \'--target_pid\' argument (%s)' % e
 
     schemes = damos_from_args(args)
 
-    return _damon.DamonCtx('0', intervals, nr_regions, ops, [target], schemes)
+    try:
+        return _damon.DamonCtx(
+                '0', intervals, nr_regions, ops, [target], schemes), None
+    except Exception as e:
+        return None, 'Creating context from arguments failed (%s)' % e
 
 def uncomment_kvpairs_str(string):
     lines = []
@@ -73,9 +89,13 @@ def kdamonds_from_damon_args(args):
         else:
             kdamonds_str = args.kdamonds
         kdamonds_kvpairs = json.loads(uncomment_kvpairs_str(kdamonds_str))
-        return [kvpairs_to_Kdamond(kvpair) for kvpair in kdamonds_kvpairs]
+        return [kvpairs_to_Kdamond(kvpair)
+                for kvpair in kdamonds_kvpairs], None
+    ctx, err = damon_ctx_from_damon_args(args)
+    if err:
+        return None, err
     return [_damon.Kdamond(name='0', state=None, pid=None,
-        contexts=[damon_ctx_from_damon_args(args)])]
+        contexts=[ctx])], None
 
 def set_implicit_target_args_explicit(args):
     args.self_started_target = False
@@ -125,7 +145,9 @@ def turn_explicit_args_damon_on(args):
 
 def turn_implicit_args_damon_on(args, record_request):
     set_implicit_target_args_explicit(args)
-    ctx = damon_ctx_from_damon_args(args)
+    ctx, err = damon_ctx_from_damon_args(args)
+    if err:
+        return err, None
     if _damon.feature_supported('record'):
         ctx.record_request = record_request
     kdamonds = [_damon.Kdamond('0', state=None, pid=None, contexts=[ctx])]

damon_args_to_json.py
@@ -13,7 +13,10 @@ def main():
 
     _damon.ensure_root_and_initialized(args)
 
-    kdamonds = _damon_args.kdamonds_from_damon_args(args)
+    kdamonds, err = _damon_args.kdamonds_from_damon_args(args)
+    if err:
+        print('creating kdamonds from arguments failed (%s)' % err)
+        exit(1)
     kvpairs = [k.to_kvpairs() for k in kdamonds]
     print(json.dumps(kvpairs, indent=4, sort_keys=True))
 

tests/unittest/test_damon_args.py
@@ -22,7 +22,9 @@ class TestDamonArgs(unittest.TestCase):
                 ('--sample 5000 --aggr 100000 --updr 1000000 ' +
                     '--minr 10 --maxr 1000 --regions=123-456 paddr').split())
         _damon_args.set_implicit_target_args_explicit(args)
-        self.assertEqual(_damon_args.damon_ctx_from_damon_args(args),
+        ctx, err = _damon_args.damon_ctx_from_damon_args(args)
+        self.assertEqual(err, None)
+        self.assertEqual(ctx,
             _damon.DamonCtx('0',
                 _damon.DamonIntervals(5000, 100000, 1000000),
                 _damon.DamonNrRegionsRange(10, 1000), 'paddr',
@@ -34,7 +36,9 @@ class TestDamonArgs(unittest.TestCase):
                 ('--sample 5ms --aggr 100ms --updr 1s ' +
                     '--minr 10 --maxr 1,000 --regions=1K-4K paddr').split())
         _damon_args.set_implicit_target_args_explicit(args)
-        self.assertEqual(_damon_args.damon_ctx_from_damon_args(args),
+        ctx, err = _damon_args.damon_ctx_from_damon_args(args)
+        self.assertEqual(err, None)
+        self.assertEqual(ctx,
             _damon.DamonCtx('0',
                 _damon.DamonIntervals(5000, 100000, 1000000),
                 _damon.DamonNrRegionsRange(10, 1000), 'paddr',
@@ -49,7 +53,9 @@ class TestDamonArgs(unittest.TestCase):
                 ('--sample 5ms --aggr 100ms --updr 1s ' +
                     '--minr 10 --maxr 1,000 --regions=1K-4K ' +
                     '--ops paddr').split())
-        self.assertEqual(_damon_args.damon_ctx_from_damon_args(args),
+        ctx, err = _damon_args.damon_ctx_from_damon_args(args)
+        self.assertEqual(err, None)
+        self.assertEqual(ctx,
             _damon.DamonCtx('0',
                 _damon.DamonIntervals(5000, 100000, 1000000),
                 _damon.DamonNrRegionsRange(10, 1000), 'paddr',

commit 924db8aa1623abe3bc05554cbec3b0c37b525942

damo_fmt_json: Fix wrong kdamonds_from_args() error handling
damo_fmt_json.py is not receiving the error from kdamonds_from_args()
but from list comprehension.  Fix it.

Fixes: e448af8f041d ("_damon_args: damon_ctx_from_damon_args(): Return error")

Signed-off-by: SeongJae Park <sj38.park@gmail.com>

damo_fmt_json.py
@@ -21,11 +21,10 @@ def main(args=None):
         set_argparser(parser)
         args = parser.parse_args()
 
-    kdamonds, err = [k.to_kvpairs(args.raw) for k in
-            _damon_args.kdamonds_from_args(args)]
+    kdamonds, err = _damon_args.kdamonds_from_args(args)
     if err:
-        print('creating kdamonds from arguments failed (%s)' % err)
-        exit(1)
+        print('invalid arguments (%s)' % err)
+    kdamonds = [k.to_kvpairs(args.raw) for k in kdamonds]
     print(json.dumps(kdamonds, indent=4))
 
 if __name__ == '__main__':