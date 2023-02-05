8eee09b8b62c20e53580044783de49a75f5504d0
SeongJae Park <sj38.park@gmail.com>
2023-02-05 19:54:11 +0000

    _test_damo_common: Implement damo dir syspath setup
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unit/_test_damo_common.py
+++ tests/unit/_test_damo_common.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0
 
+import os
+import sys
+
 def test_input_expects(testcase, function, input_expects):
     for input_ in input_expects:
         testcase.assertEqual(function(input_), input_expects[input_])
@@ -9,3 +12,8 @@ def test_input_expects_funcs(testcase, functions, input_expects):
     for input_ in input_expects:
         for idx, expect in enumerate(input_expects[input_]):
             test_input_expects(testcase, functions[idx], {input_: expect})
+
+def add_damo_dir_to_syspath():
+    bindir = os.path.dirname(os.path.realpath(__file__))
+    damo_dir = os.path.join(bindir, '..', '..')
+    sys.path.append(damo_dir)
