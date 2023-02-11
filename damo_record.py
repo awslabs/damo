fada39408d7c6607979a44b069297158ad4b930b
SeongJae Park <sj38.park@gmail.com>
2023-02-11 20:51:42 +0000

    damo_record/change_rfile_format(): Remove unnecessary intermediate file creation
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_record.py
+++ damo_record.py
@@ -27,15 +27,12 @@ class DataForCleanup:
 data_for_cleanup = DataForCleanup()
 
 def change_rfile_format(rfile_path, dst_format, dst_permission):
-    rfile_path_mid = rfile_path + '.mid'
-    os.rename(rfile_path, rfile_path_mid)
-    result, err = _damon_result.parse_damon_result(rfile_path_mid, None)
+    result, err = _damon_result.parse_damon_result(rfile_path, None)
     if err:
         raise Exception('parsing DAMON output failed (%s)' % err)
 
     _damon_result.write_damon_result(result, rfile_path, dst_format,
             dst_permission)
-    os.remove(rfile_path_mid)
 
 def cleanup_exit(exit_code):
     if not data_for_cleanup.target_is_ongoing:
