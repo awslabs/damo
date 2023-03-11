853cb03f64f8ab26a0a34a59de0897bfdaea900c
SeongJae Park <sj38.park@gmail.com>
2023-03-11 20:09:40 +0000

    damo_stat_regions: Add priority-based sorting option
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_stat_regions.py
+++ damo_stat_regions.py
@@ -9,7 +9,17 @@ import _damo_fmt_str
 import _damo_subcmds
 import _damon
 
-def __pr_schemes_tried_regions(regions, intervals, size_only, raw_nr):
+def priority(region, weights):
+    if region.nr_accesses > 0:
+        return region.nr_accesses * weights[0] + region.age * weights[1]
+    return region.age * weights[1] * -1
+
+def __pr_schemes_tried_regions(regions, intervals, size_only, sortby,
+        prio_weights, raw_nr):
+
+    if sortby == 'priority':
+        regions.sort(key=lambda region: priority(region, prio_weights))
+
     total_sz = 0
     for region in regions:
         if not size_only:
@@ -19,14 +29,16 @@ def __pr_schemes_tried_regions(regions, intervals, size_only, raw_nr):
     if size_only:
         print('%s' % _damo_fmt_str.format_sz(total_sz, raw_nr))
 
-def pr_schemes_tried_regions(monitor_scheme, size_only, raw_nr):
+def pr_schemes_tried_regions(monitor_scheme, size_only, sortby, prio_weights,
+        raw_nr):
     for kdamond in _damon.running_kdamonds():
         for ctx in kdamond.contexts:
             print('kdamond %s ctx %s' % (kdamond.name, ctx.name))
             for scheme in ctx.schemes:
                 if scheme.effectively_equal(monitor_scheme, ctx.intervals):
                     __pr_schemes_tried_regions(scheme.tried_regions,
-                            ctx.intervals, size_only, raw_nr)
+                            ctx.intervals, size_only, sortby, prio_weights,
+                            raw_nr)
                     break
 
 def install_scheme_if_needed(kdamonds, scheme_to_install):
@@ -48,7 +60,8 @@ def install_scheme_if_needed(kdamonds, scheme_to_install):
                     'committing scheme installed kdamonds failed: %s' % err)
     return installed, None
 
-def update_pr_schemes_tried_regions(monitor_scheme, size_only, raw_nr):
+def update_pr_schemes_tried_regions(monitor_scheme, size_only, sortby,
+        prio_weights, raw_nr):
     orig_kdamonds = _damon.current_kdamonds()
     running_kdamonds = _damon.running_kdamonds()
     if len(running_kdamonds) == 0:
@@ -71,7 +84,8 @@ def update_pr_schemes_tried_regions(monitor_scheme, size_only, raw_nr):
                 print('monitoring scheme uninstall failed: %s' % err)
             return
 
-    pr_schemes_tried_regions(monitor_scheme, size_only, raw_nr)
+    pr_schemes_tried_regions(monitor_scheme, size_only, sortby, prio_weights,
+            raw_nr)
 
     if installed:
         err = _damon.commit(orig_kdamonds)
@@ -91,10 +105,17 @@ def set_argparser(parser):
             help='min/max age of regions (microseconds)')
     parser.add_argument('--size_only', action='store_true',
             help='print total size only')
+    parser.add_argument('--sortby', choices=['address', 'priority'],
+            default='sortby',
+            help='the metric for the regions print order')
+    parser.add_argument('--priority_weights', nargs=2, type=float,
+            default=[1, 1,],
+            metavar=('<access rate weight>, <age weight>'),
+            help='priority weights for priority calculation')
 
 def __main(args):
     update_pr_schemes_tried_regions(args.monitor_scheme, args.size_only,
-            args.raw)
+            args.sortby, args.priority_weights, args.raw)
 
 def main(args=None):
     if not args:
