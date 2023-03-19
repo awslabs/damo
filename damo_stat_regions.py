15f8924ba56060de90cd4288f849f5a2ad366b0c
SeongJae Park <sj38.park@gmail.com>
2023-03-19 17:57:19 +0000

    damo_stat_regions: Use _damon_result.get_snapshots()
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_stat_regions.py
+++ damo_stat_regions.py
@@ -8,6 +8,7 @@ import damo_stat
 import _damo_fmt_str
 import _damo_subcmds
 import _damon
+import _damon_result
 
 def priority(region, weights):
     if region.nr_accesses > 0:
@@ -22,6 +23,8 @@ def __pr_schemes_tried_regions(regions, intervals, size_only, sortby,
 
     total_sz = 0
     for region in regions:
+        region = _damon.DamosTriedRegion(region.start, region.end,
+                region.nr_accesses, region.age)
         if not size_only:
             print(region.to_str(raw_nr, intervals))
         else:
@@ -29,68 +32,19 @@ def __pr_schemes_tried_regions(regions, intervals, size_only, sortby,
     if size_only:
         print('%s' % _damo_fmt_str.format_sz(total_sz, raw_nr))
 
-def pr_schemes_tried_regions(monitor_scheme, size_only, sortby, prio_weights,
-        raw_nr):
-    for kdamond in _damon.running_kdamonds():
-        for ctx in kdamond.contexts:
-            print('kdamond %s ctx %s' % (kdamond.name, ctx.name))
-            for scheme in ctx.schemes:
-                if scheme.effectively_equal(monitor_scheme, ctx.intervals):
-                    __pr_schemes_tried_regions(scheme.tried_regions,
-                            ctx.intervals, size_only, sortby, prio_weights,
-                            raw_nr)
-                    break
-
-def install_scheme_if_needed(kdamonds, scheme_to_install):
-    installed = False
-    for kdamond in kdamonds:
-        for ctx in kdamond.contexts:
-            ctx_has_the_scheme = False
-            for scheme in ctx.schemes:
-                if scheme.effectively_equal(scheme_to_install, ctx.intervals):
-                    ctx_has_the_scheme = True
-                    break
-            if not ctx_has_the_scheme:
-                ctx.schemes.append(scheme_to_install)
-                installed = True
-    if installed:
-        err = _damon.commit(kdamonds)
-        if err != None:
-            return (False,
-                    'committing scheme installed kdamonds failed: %s' % err)
-    return installed, None
-
 def update_pr_schemes_tried_regions(monitor_scheme, size_only, sortby,
         prio_weights, raw_nr):
-    orig_kdamonds = _damon.current_kdamonds()
-    running_kdamonds = _damon.running_kdamonds()
-    if len(running_kdamonds) == 0:
-        print('no kdamond running')
+    snapshots, err = _damon_result.get_snapshots(monitor_scheme.access_pattern)
+    if snapshots == None:
+        print(err)
         return
 
-    # ensure each kdamonds have a monitoring scheme
-    installed, err = install_scheme_if_needed(running_kdamonds, monitor_scheme)
-    if err:
-        print('monitoring scheme install failed: %s' % err)
-        return
-
-    err = _damon.update_schemes_tried_regions([k.name for k in
-        running_kdamonds])
-    if err != None:
-        print('update schemes tried regions fail: %s' % err)
-        if installed:
-            err = _damon.commit(orig_kdamonds)
-            if err:
-                print('monitoring scheme uninstall failed: %s' % err)
-            return
-
-    pr_schemes_tried_regions(monitor_scheme, size_only, sortby, prio_weights,
-            raw_nr)
-
-    if installed:
-        err = _damon.commit(orig_kdamonds)
-        if err:
-            print('monitoring scheme uninstall failed: %s' % err)
+    for kdamond_name, kdamond_snapshots in snapshots.items():
+        for ctx_name, ctx_snapshot in kdamond_snapshots[1].items():
+            print('kdamond %s ctx %s' % (kdamond_name, ctx_name))
+            ctx, snapshot = ctx_snapshot
+            __pr_schemes_tried_regions(snapshot.regions, ctx.intervals,
+                    size_only, sortby, prio_weights, raw_nr)
 
 def set_argparser(parser):
     damo_stat.set_common_argparser(parser)
