4af7a4bfd3ce69acec57de95ae2d2f9d5a82575a
SeongJae Park <sj38.park@gmail.com>
2023-03-25 19:14:02 +0000

    _damon_args: Mark --damon_interface as deprecated
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damon_args.py
+++ _damon_args.py
@@ -198,7 +198,8 @@ def turn_damon_on(args):
 def set_common_argparser(parser):
     parser.add_argument('--damon_interface',
             choices=['sysfs', 'debugfs', 'auto'],
-            default='auto', help='underlying DAMON interface to use')
+            default='auto',
+            help='underlying DAMON interface to use (!! DEPRECATED)')
     parser.add_argument('--debug_damon', action='store_true',
             help='Print debugging log')
 
