3240d68e7284442e1ef0c718f485fddfed148a77
SeongJae Park <sj38.park@gmail.com>
2023-04-01 19:13:14 +0000

    damo_stat_kdamonds: Use update_schemes_status() and current_kdamonds()
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_stat_kdamonds.py
+++ damo_stat_kdamonds.py
@@ -18,10 +18,11 @@ def update_pr_kdamonds_summary(json_format, raw_nr):
     print('\n'.join(summary))
 
 def update_pr_kdamonds(json_format, raw_nr):
-    kdamonds, err = _damon.schemes_updated_kdamonds()
-    if err != None:
+    err = _damon.update_schemes_stats()
+    if err:
         print(err)
         return
+    kdamonds = _damon.current_kdamonds()
     if json_format:
         print(json.dumps([k.to_kvpairs(raw_nr) for k in kdamonds], indent=4))
     else:
