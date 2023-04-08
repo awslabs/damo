d322e3c4ff1dfec272deca7879dae64ec0194833
SeongJae Park <sj38.park@gmail.com>
2023-04-08 19:35:37 +0000

    Add a subcommand for old .damos to new .json DAMOS translation
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ damo_translate_damos.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Convert old damos schemes to json format.
+"""
+
+import argparse
+import json
+
+import _damon_args_schemes
+
+def set_argparser(parser):
+    parser.add_argument('--schemes', metavar='<file or string>',
+            help='schemes in old .damos format')
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    schemes, err = _damon_args_schemes.damo_schemes_to_damos(args.schemes)
+    if err:
+        print('failed --schemes parsing (%s)' % err)
+        exit(1)
+    print(json.dumps([scheme.to_kvpairs() for scheme in schemes], indent=4))
+
+if __name__ == '__main__':
+    main()
