6f6bab92e64dc7e5737181d849636bdf2e268a9c
SeongJae Park <sj38.park@gmail.com>
2023-04-29 19:43:15 +0000

    _damo_python2_support: Announce Python2 support as deprecated
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damo_python2_support.py
+++ _damo_python2_support.py
@@ -7,9 +7,10 @@ import sys
 
 if sys.version.startswith('2.'):
     sys.stderr.write('''
-WARNING: damo will remove python2 support by 2023-Q2.  Please report your
-    usecase to sj@kernel.org, damon@lists.linux.dev and linux-mm@kvack.org if
-    you depend on those.
+Python2 support of damo is deprecated.  This will not work suddenly.
+
+Please report your usecase to sj@kernel.org, damon@lists.linux.dev and
+linux-mm@kvack.org if you depend on those.
 
 ''')
 
