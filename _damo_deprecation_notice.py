0fc44f080b473158b53c579f381578526166988a
SeongJae Park <sj38.park@gmail.com>
2023-05-21 18:45:37 +0000

    _damo_deprecation_notice: Let caller exit the program
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damo_deprecation_notice.py
+++ _damo_deprecation_notice.py
@@ -2,10 +2,13 @@
 
 import sys
 
-def deprecated(feature, deadline, additional_notice=''):
+def deprecated(feature, deadline, do_exit=False, exit_code=1,
+        additional_notice=''):
     sys.stderr.write('\n'.join([
 'WARNING: %s is deprecated.' % feature,
 '    The support will be removed by %s.' % deadline,
 '    %s' % additional_notice,
 '    Please report your usecase to sj@kernel.org, damon@lists.linux.dev and',
 '    linux-mm@kvack.org if you depend on those.']))
+    if do_exit:
+        exit(exit_code)
