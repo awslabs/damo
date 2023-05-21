58bc0046a96c580f0412175a8e2f3224c6bdce4b
SeongJae Park <sj38.park@gmail.com>
2023-05-21 18:36:57 +0000

    _damo_deprecation_notice/deprecated(): Support additional notice
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damo_deprecation_notice.py
+++ _damo_deprecation_notice.py
@@ -2,9 +2,10 @@
 
 import sys
 
-def deprecated(feature, deadline):
+def deprecated(feature, deadline, additional_notice=''):
     sys.stderr.write('\n'.join([
 'WARNING: %s is deprecated.' % feature,
 '    The support will be removed by %s.' % deadline,
+'    %s' % additional_notice,
 '    Please report your usecase to sj@kernel.org, damon@lists.linux.dev and',
 '    linux-mm@kvack.org if you depend on those.']))
