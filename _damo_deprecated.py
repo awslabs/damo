4b8e4de23842564bc6b6beb5fa5822591d957edc
SeongJae Park <sj38.park@gmail.com>
2023-06-04 17:35:48 +0000

    _damo_python2_support: Move the code to _damo_deprecated.py
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damo_deprecated.py
+++ _damo_deprecated.py
@@ -7,11 +7,40 @@ e.g., 'translate_damos' and 'convert_record_format'.
 
 import json
 import os
+import subprocess
+import sys
 
 import _damon
 
 import _damo_deprecation_notice
 import _damo_fmt_str
+
+'''
+Python2 support
+'''
+
+if sys.version.startswith('2.'):
+    _damo_deprecation_notice.deprecated(feature='Python2 support of damo',
+            deadline='2023-Q2')
+
+# For supporting python 2.6
+try:
+    subprocess.DEVNULL = subprocess.DEVNULL
+except AttributeError:
+    subprocess.DEVNULL = open(os.devnull, 'wb')
+
+try:
+    subprocess.check_output = subprocess.check_output
+except AttributeError:
+    def check_output(*popenargs, **kwargs):
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, err = process.communicate()
+        rc = process.poll()
+        if rc:
+            raise subprocess.CalledProcessError(rc, popenargs[0])
+        return output
+
+    subprocess.check_output = check_output
 
 '''
 DAMOS single-line scheme specification input.
