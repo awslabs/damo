78db20b5596b6d26cfe0d5d256d488aa42eb9da5
SeongJae Park <sj38.park@gmail.com>
2023-06-10 19:08:39 +0000

    damo_show_status: Implement kdamonds target
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_show_status.py
+++ damo_show_status.py
@@ -4,13 +4,26 @@
 Show status of DAMON.
 """
 
+import _damon
+import _damon_args
+
+import damo_stat_kdamonds
+
 def set_argparser(parser):
+    parser.add_argument('target', metavar='<target>', choices=['kdamonds'],
+            help='what status to show')
+    _damon_args.set_common_argparser(parser)
     return parser
 
 def main(args=None):
     if not args:
         parser = set_argparser(parser)
         args = parser.parse_args()
+
+    _damon.ensure_root_and_initialized(args)
+
+    if args.target == 'kdamonds':
+        damo_stat_kdamonds.update_pr_kdamonds_summary(False, False)
 
 if __name__ == '__main__':
     main()
