awslabs/damo

commit 14e104e1dfb65fb0dd94076f8f3691a7f073eb30

_damon_result: Put 'intervals' field from DamonRecord.to_kvpairs() always
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_result.py
@@ -84,8 +84,8 @@ class DamonRecord:
         ordered_dict = collections.OrderedDict()
         ordered_dict['kdamond_idx'] = self.kdamond_idx
         ordered_dict['context_idx'] = self.context_idx
-        if self.intervals != None:
-            ordered_dict['intervals'] = self.intervals.to_kvpairs(raw)
+        ordered_dict['intervals'] = self.intervals.to_kvpairs(raw)
+                                    if self.intervals != None else None
         ordered_dict['scheme_idx'] = self.scheme_idx
         ordered_dict['target_id'] = self.target_id
         ordered_dict['snapshots'] = [s.to_kvpairs(raw) for s in self.snapshots]

commit efbca4e3fed364e887f7b4b5939bb5b6026b0c0c

_damon_result: Fix wrong indentation
Fixes: 14e104e1dfb6 ("_damon_result: Put 'intervals' field from DamonRecord.to_kvpairs() always")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_result.py
@@ -84,8 +84,8 @@ class DamonRecord:
         ordered_dict = collections.OrderedDict()
         ordered_dict['kdamond_idx'] = self.kdamond_idx
         ordered_dict['context_idx'] = self.context_idx
-        ordered_dict['intervals'] = self.intervals.to_kvpairs(raw)
-                                    if self.intervals != None else None
+        ordered_dict['intervals'] = (self.intervals.to_kvpairs(raw)
+                if self.intervals != None else None)
         ordered_dict['scheme_idx'] = self.scheme_idx
         ordered_dict['target_id'] = self.target_id
         ordered_dict['snapshots'] = [s.to_kvpairs(raw) for s in self.snapshots]