089510a1edcf49e45a7f040e5bf9b1dde039514e
SeongJae Park <sj38.park@gmail.com>
2023-06-17 19:51:08 +0000

    damo_report_raw: Handle BrokenPipeError from printing output
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_report_raw.py
+++ damo_report_raw.py
@@ -40,49 +40,54 @@ def main(args=None):
         print('no monitoring result in the file')
         exit(1)
 
-    if args.json:
-        print(json.dumps([r.to_kvpairs(args.raw_number)
-            for r in result.records], indent=4))
-        exit(0)
+    try:
+        if args.json:
+            print(json.dumps([r.to_kvpairs(args.raw_number)
+                for r in result.records], indent=4))
+            exit(0)
 
-    for record in result.records:
-        snapshots = record.snapshots
-        if len(snapshots) == 0:
-            continue
+        for record in result.records:
+            snapshots = record.snapshots
+            if len(snapshots) == 0:
+                continue
 
-        base_time = snapshots[0].start_time
-        print('base_time_absolute: %s\n' %
-                _damo_fmt_str.format_time_ns(base_time, args.raw_number))
+            base_time = snapshots[0].start_time
+            print('base_time_absolute: %s\n' %
+                    _damo_fmt_str.format_time_ns(base_time, args.raw_number))
 
-        for snapshot in snapshots:
-            if args.duration:
-                time_offset_sec = (snapshot.start_time - base_time) / 1000000000
-                if time_offset_sec < args.duration[0]:
-                    continue
-                if time_offset_sec > args.duration[1]:
-                    break
-            print('monitoring_start:    %16s' %
-                    _damo_fmt_str.format_time_ns(
-                        snapshot.start_time - base_time, args.raw_number))
-            print('monitoring_end:      %16s' %
-                    _damo_fmt_str.format_time_ns(
-                        snapshot.end_time - base_time, args.raw_number))
-            print('monitoring_duration: %16s' %
-                    _damo_fmt_str.format_time_ns(
-                        snapshot.end_time - snapshot.start_time,
-                        args.raw_number))
-            print('target_id: %s' % record.target_id)
-            print('nr_regions: %s' % len(snapshot.regions))
-            print('# %10s %12s  %12s  %11s %5s' %
-                    ('start_addr', 'end_addr', 'length', 'nr_accesses', 'age'))
-            for r in snapshot.regions:
-                print("%012x-%012x (%12s) %11d %5d" %
-                        (r.start, r.end,
-                            _damo_fmt_str.format_sz(r.end - r.start,
-                                args.raw_number), r.nr_accesses.samples,
-                                r.age.aggr_intervals
-                                if r.age.aggr_intervals != None else -1))
-            print('')
+            for snapshot in snapshots:
+                if args.duration:
+                    time_offset_sec = (snapshot.start_time - base_time) / 1000000000
+                    if time_offset_sec < args.duration[0]:
+                        continue
+                    if time_offset_sec > args.duration[1]:
+                        break
+                print('monitoring_start:    %16s' %
+                        _damo_fmt_str.format_time_ns(
+                            snapshot.start_time - base_time, args.raw_number))
+                print('monitoring_end:      %16s' %
+                        _damo_fmt_str.format_time_ns(
+                            snapshot.end_time - base_time, args.raw_number))
+                print('monitoring_duration: %16s' %
+                        _damo_fmt_str.format_time_ns(
+                            snapshot.end_time - snapshot.start_time,
+                            args.raw_number))
+                print('target_id: %s' % record.target_id)
+                print('nr_regions: %s' % len(snapshot.regions))
+                print('# %10s %12s  %12s  %11s %5s' %
+                        ('start_addr', 'end_addr', 'length', 'nr_accesses', 'age'))
+                for r in snapshot.regions:
+                    print("%012x-%012x (%12s) %11d %5d" %
+                            (r.start, r.end,
+                                _damo_fmt_str.format_sz(r.end - r.start,
+                                    args.raw_number), r.nr_accesses.samples,
+                                    r.age.aggr_intervals
+                                    if r.age.aggr_intervals != None else -1))
+                print('')
+    except BrokenPipeError as e:
+        # maybe user piped to 'less' like pager and quit from it
+        pass
+
 
 if __name__ == '__main__':
     main()
