9862a0a6a9fda0013ac543c57c33a0b90e6c5b7f
SeongJae Park <sj38.park@gmail.com>
2023-07-22 18:09:40 +0000

    tests/unittest: Add a test for damo_show.filter_by_addr
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ tests/unit/test_damo_show.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import unittest
+
+import _test_damo_common
+
+_test_damo_common.add_damo_dir_to_syspath()
+
+import _damon
+import damo_show
+
+class TestDamoShow(unittest.TestCase):
+    def test_filter_by_address(self):
+        ranges = [[3, 5], [6, 9], [10, 12]]
+        region = _damon.DamonRegion(7, 8)
+        self.assertEqual(
+                damo_show.filter_by_addr(_damon.DamonRegion(7, 8), ranges),
+                [_damon.DamonRegion(7, 8)])
+        self.assertEqual(
+                damo_show.filter_by_addr(_damon.DamonRegion(1, 2), ranges),
+                [])
+        self.assertEqual(
+                damo_show.filter_by_addr(_damon.DamonRegion(5, 6), ranges),
+                [])
+        self.assertEqual(
+                damo_show.filter_by_addr(_damon.DamonRegion(1, 4), ranges),
+                [_damon.DamonRegion(3, 4)])
+        self.assertEqual(
+                damo_show.filter_by_addr(_damon.DamonRegion(1, 20), ranges),
+                [_damon.DamonRegion(3, 5), _damon.DamonRegion(6, 9),
+                    _damon.DamonRegion(10, 12)])
+
+if __name__ == '__main__':
+    unittest.main()
