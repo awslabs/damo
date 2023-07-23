c5c678fa3d9134ac662062653137002ed498450f
SeongJae Park <sj38.park@gmail.com>
2023-07-23 17:48:54 +0000

    tests/unit/test_damon_result: Test DamonRecord.from_kvpairs()
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unit/test_damon_result.py
+++ tests/unit/test_damon_result.py
@@ -27,5 +27,40 @@ class TestDamon(unittest.TestCase):
         self.assertIsNone(perm)
         self.assertIsNotNone(err)
 
+    def test_record_from_kvpairs(self):
+        records = [_damon_result.DamonRecord.from_kvpairs(p) for p in [
+            {
+                "kdamond_idx": 0, "context_idx": 0,
+                "intervals": {
+                    "sample_us": "5ms", "aggr_us": "100ms",
+                    "ops_update_us": "1s"
+                    },
+                "scheme_idx": 0, "target_id": None,
+                "snapshots": [
+                    {
+                        "start_time": "1690134099856513792",
+                        "end_time": "1690134099956513792",
+                        "regions": [
+                            {
+                                "start": "4,294,967,296",
+                                "end": "7,516,192,768",
+                                "nr_accesses": {
+                                    "samples": 0,
+                                    "percent": None
+                                    },
+                                "age": {
+                                    "usec": None,
+                                    "aggr_intervals": "5,913"
+                                    }
+                                }
+                            ],
+                        "total_bytes": "3221225472"
+                        }
+                    ]
+                }
+            ]]
+        self.assertEqual(len(records), 1)
+        self.assertEqual(len(records[0].snapshots), 1)
+
 if __name__ == '__main__':
     unittest.main()
