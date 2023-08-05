8e95241d8a42109dff9d236f7a3583ba18b38c45
SeongJae Park <sj38.park@gmail.com>
2023-08-05 18:28:24 +0000

    damo_show: Implement ascii coloring code
    Implement code for ascii coloring.  The code is mostly stollen from
    damo_heats.py.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_show.py
+++ damo_show.py
@@ -151,6 +151,27 @@ def size_bar(region, minmaxs, min_cols, max_cols):
             [min_cols, max_cols]))
     return '<%s>' % ('-' * nr_cols)
 
+
+# {name: [[background colors], [foreground colors]]}
+colorsets = {
+    'gray':[
+        [232] * 10,
+        [237, 239, 241, 243, 245, 247, 249, 251, 253, 255]],
+    'flame':[
+        [232, 1, 1, 2, 3, 3, 20, 21,26, 27, 27],
+        [239, 235, 237, 239, 243, 245, 247, 249, 251, 255]],
+    'emotion':[
+        [232, 234, 20, 21, 26, 2, 3, 1, 1, 1],
+        [239, 235, 237, 239, 243, 245, 247, 249, 251, 255]],
+    }
+
+def colored(txt, colorset, level):
+    bg = colorset[0][level]
+    fg = colorset[1][level]
+    color_prefix = u'\u001b[48;5;%dm\u001b[38;5;%dm' % (bg, fg)
+    color_suffix = u'\u001b[0m'
+    return color_prefix + txt + color_suffix
+
 def size_heat_bar(region, minmaxs, min_cols, max_cols):
     nr_cols = int(rescale_val(region.size(),
             [minmaxs.min_sz_region, minmaxs.max_sz_region],
