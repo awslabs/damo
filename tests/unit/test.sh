fa1b84bd349cf917902bbffa5bc3594e197bb3e7
SeongJae Park <sj38.park@gmail.com>
2023-08-26 01:28:44 +0000

    tests/unit/test: Remove legacy of Python2 support
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tests/unit/test.sh
+++ tests/unit/test.sh
@@ -5,14 +5,11 @@ bindir=$(dirname "$0")
 
 for test_file in "$bindir"/test_*.py
 do
-	for py in "python3"
-	do
-		if "$py" "$test_file" &> /dev/null
-		then
-			echo "PASS unit-$py $(basename $test_file)"
-		else
-			echo "FAIL unit-$py $(basename $test_file)"
-			exit 1
-		fi
-	done
+	if python3 "$test_file" &> /dev/null
+	then
+		echo "PASS unit $(basename $test_file)"
+	else
+		echo "FAIL unit $(basename $test_file)"
+		exit 1
+	fi
 done
