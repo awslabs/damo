awslabs/damo

commit 07c8a6a5847895b9a357563b6a50a25618d4273c

_damon_sysfs: Support DAMOS apply intervals
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -173,6 +173,7 @@ def wops_for_schemes(ctx):
             'access_pattern': wops_for_scheme_access_pattern(
                 scheme.access_pattern, ctx),
             'action': scheme.action,
+            'apply_interval_us': '%s' % scheme.apply_interval_us,
             'quotas': wops_for_scheme_quotas(scheme.quotas),
             'watermarks': wops_for_scheme_watermarks(scheme.watermarks),
         }
@@ -392,7 +393,8 @@ def files_content_to_scheme(files_content):
     return _damon.Damos(
             files_content_to_access_pattern(files_content['access_pattern']),
             files_content['action'].strip(),
-            None,
+            files_content['apply_interval_us'].strip()
+                if 'apply_interval_us' in files_content else None,
             files_content_to_quotas(files_content['quotas']),
             files_content_to_watermarks(files_content['watermarks']),
             files_content_to_damos_filters(files_content['filters'])

tests/unit/test_damon_sysfs.py
@@ -111,6 +111,7 @@ class TestDamonSysfs(unittest.TestCase):
                                             "sz": {"max": "0", "min": "0"}
                                             },
                                         "action": "stat",
+                                        "apply_interval_us": "0",
                                         "quotas": {
                                             "bytes": "0", "ms": "0",
                                             "reset_interval_ms": "0",

commit b5ae1c11b5dd232c01309361dda698380627cc9a

_damon: Add schemes_apply_interval feature
DAMOS apply interval is tried on every kernel, when sysfs interface is
used.  Since the feature is only available at damon/next tree as of this
writing, it fails.  Fix it by adding a feature and check its
availablility from _damon_sysfs.

Fixes: 07c8a6a58478 ("_damon_sysfs: Support DAMOS apply intervals")
Reported-by: Honggyu Kim <honggyu.kp@gmail.com>
Link: https://github.com/awslabs/damo/issues/75
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon.py
@@ -882,6 +882,7 @@ features = ['record',       # was in DAMON patchset, but not merged in mainline
             'schemes_tried_regions',    # merged in v6.2-rc1
             'schemes_filters',          # merged in v6.3-rc1
             'schemes_tried_regions_sz', # developing
+            'schemes_apply_interval',   # developing on v6.5
             ]
 
 _damon_fs = None

_damon_sysfs.py
@@ -173,13 +173,15 @@ def wops_for_schemes(ctx):
             'access_pattern': wops_for_scheme_access_pattern(
                 scheme.access_pattern, ctx),
             'action': scheme.action,
-            'apply_interval_us': '%d' % scheme.apply_interval_us,
             'quotas': wops_for_scheme_quotas(scheme.quotas),
             'watermarks': wops_for_scheme_watermarks(scheme.watermarks),
         }
         if feature_supported('schemes_filters'):
             schemes_wops[dirname]['filters'] = wops_for_scheme_filters(
                     scheme.filters)
+        if feature_supported('schemes_apply_interval'):
+            schemes_wops[dirname]['apply_interval_us'] = (
+                    '%d' % scheme.apply_interval_us)
     return schemes_wops
 
 def wops_for_regions(regions):
@@ -549,6 +551,9 @@ def update_supported_features():
 
     if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'filters')):
         feature_supports['schemes_filters'] = True
+
+    if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'apply_interval_us')):
+        feature_supports['schemes_apply_interval'] = True
 
     avail_ops, err = _avail_ops()
     if err == None:

tests/unit/test_damon_sysfs.py
@@ -24,6 +24,7 @@ class TestDamonSysfs(unittest.TestCase):
                 'schemes_tried_regions': True,
                 'schemes_wmarks': True,
                 'schemes_filters': True,
+                'schemes_apply_interval': True,
                 'vaddr': True}
 
         sysfs_dict = {