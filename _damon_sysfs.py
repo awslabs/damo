awslabs/damo

commit f050eda66867dd47d432cb90098b7af0dc5bfd3c

_damon: Add schemes_tried_regions_sz feature
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -538,6 +538,10 @@ def update_supported_features():
     if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'tried_regions')):
         feature_supports['schemes_tried_regions'] = True
 
+    if os.path.isfile(os.path.join(scheme_dir_of(0, 0, 0), 'tried_regions',
+            'sz_regions_sum')):
+        feature_supports['schemes_tried_regions_sz'] = True
+
     if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'filters')):
         feature_supports['schemes_filters'] = True
 

_damon.py
@@ -783,6 +783,7 @@ features = ['record',       # was in DAMON patchset, but not merged in mainline
             'fvaddr',       # merged in v5.19 (b82434471cd2)
             'schemes_tried_regions',    # merged in v6.2-rc1
             'schemes_filters',          # merged in mm-unstable
+            'schemes_tried_regions_sz', # developing
             ]
 
 _damon_fs = None

commit 8a08d61782b7b7b2aa365acc0f97221416b25ee2

_damon_sysfs: Fix wrong DAMOS tried regions size file name
The DAMOS tried regions size feature check logic is not using proper
file name.  Fix it.

Fixes: f050eda66867 ("_damon: Add schemes_tried_regions_sz feature")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -544,7 +544,7 @@ def update_supported_features():
         feature_supports['schemes_tried_regions'] = True
 
     if os.path.isfile(os.path.join(scheme_tried_regions_dir_of(0, 0, 0),
-            'sz_regions_sum')):
+            'total_bytes')):
         feature_supports['schemes_tried_regions_sz'] = True
 
     if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'filters')):