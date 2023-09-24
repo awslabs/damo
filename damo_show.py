awslabs/damo

commit 4c921c624718b3b1fee91646fe2b658e9b8980f5

damo_show: Define and use record formatters
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

damo_show.py
@@ -24,6 +24,23 @@ def apply_min_chars(min_chars, field_name, txt):
             return txt
     return txt
 
+record_formatters = {
+        '<kdamond index>': lambda record, raw:
+            '%s' % record.kdamond_idx,
+        '<context index>': lambda record, raw:
+            '%s' % record.context_idx,
+        '<scheme index>': lambda record, raw:
+            '%s' % record.scheme_idx,
+        '<target id>': lambda record, raw:
+            '%s' % record.target_id,
+        '<record start abs time>': lambda record, raw:
+            _dmo_fmt_str.format_ns(record.snapshots[0].start_time, raw),
+        '<record duration>': lambda record, raw:
+            _damo_fmt_str.format_time_ns(
+                record.snapshots[-1].end_time - record.snapshots[0].start_time,
+                raw)
+            }
+
 snapshot_formatters = {
         '<total bytes>': lambda snapshot, record, raw:
             _damo_fmt_str.format_sz(snapshot.total_bytes, raw),
@@ -57,27 +74,14 @@ region_formatters = {
             _damo_fmt_str.format_time_us(region.age.usec, raw)
         }
 
-def format_field(field_name, index, region, snapshot, record, raw):
-    # for record
-    if field_name == '<kdamond index>':
-        return '%s' % record.kdamond_idx
-    elif field_name == '<context index>':
-        return '%s' % record.context_idx
-    elif field_name == '<scheme index>':
-        return '%s' % record.scheme_idx
-    elif field_name == '<target id>':
-        return '%s' % record.target_id
-    elif field_name == '<record start abs time>':
-        return _damo_fmt_str.format_time_ns(record.snapshots[0].start_time, raw)
-    elif field_name == '<record duration>':
-        return _damo_fmt_str.format_time_ns(
-                record.snapshots[-1].end_time - record.snapshots[0].start_time,
-                raw)
-    else:
-        print(field_name)
-        raise(Exception)
-
 def format_pretty(template, min_chars, index, region, snapshot, record, raw):
+    for field_name, formatter in record_formatters.items():
+        if template.find(field_name) == -1:
+            continue
+        txt = formatter(record, raw)
+        txt = apply_min_chars(min_chars, field_name, txt)
+        template = template.replace(field_name, txt)
+
     for field_name, formatter in snapshot_formatters.items():
         if template.find(field_name) == -1:
             continue
@@ -89,15 +93,6 @@ def format_pretty(template, min_chars, index, region, snapshot, record, raw):
         if template.find(field_name) == -1:
             continue
         txt = region_formatters[field_name](index, region, raw)
-        txt = apply_min_chars(min_chars, field_name, txt)
-        template = template.replace(field_name, txt)
-    for field_name in [
-            # for record pretty
-            '<kdamond index>', '<context index>', '<scheme index>',
-            '<target id>', '<record start abs time>', '<record duration>']:
-        if template.find(field_name) == -1:
-            continue
-        txt = format_field(field_name, index, region, snapshot, record, raw)
         txt = apply_min_chars(min_chars, field_name, txt)
         template = template.replace(field_name, txt)
     template = template.replace('\\n', '\n')

commit b9f1f1698b4c9b6cfdadada8e11f2f414540b748

damo_show: Fix typo on _damo_fmt_str.format_time_ns() call
Commit 4c921c624718 ("damo_show: Define and use record formatters") made
a couple of typos on _damo_fmt_str.format_time_ns() call from record
head/tail formatter.  The problem was not found since the test doesn't
check it.  Fix those.

Fixes: 4c921c624718 ("damo_show: Define and use record formatters")
Reported-by: Honggyu Kim <honggyu.kp@gmail.com>
Closes: https://github.com/awslabs/damo/issues/80
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

damo
@@ -33,7 +33,7 @@ def pr_damo_version(args_not_use):
 subcmds = [
         # DAMON control
         _damo_subcmds.DamoSubCmd(name='start', module=damo_start,
-            msg='start DAMON with given parameters'),
+            msg='start DAMON with given parameters', msg_as_desc=True),
         _damo_subcmds.DamoSubCmd(name='tune', module=damo_tune,
             msg='update input parameters of ongoing DAMON'),
         _damo_subcmds.DamoSubCmd(name='stop', module=damo_stop,

damo_show.py
@@ -43,7 +43,7 @@ record_formatters = [
             'index of the record\'s DAMON target'),
         Formatter('<abs start time>',
             lambda record, raw:
-            _dmo_fmt_str.format_ns(record.snapshots[0].start_time, raw),
+            _damo_fmt_str.format_time_ns(record.snapshots[0].start_time, raw),
             'absolute time of the start of the record'),
         Formatter('<duration>',
             lambda record, raw: