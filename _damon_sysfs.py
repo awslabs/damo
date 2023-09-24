awslabs/damo

commit b5ae1c11b5dd232c01309361dda698380627cc9a

_damon: Add schemes_apply_interval feature
DAMOS apply interval is tried on every kernel, when sysfs interface is
used.  Since the feature is only available at damon/next tree as of this
writing, it fails.  Fix it by adding a feature and check its
availablility from _damon_sysfs.

Fixes: 07c8a6a58478 ("_damon_sysfs: Support DAMOS apply intervals")
Reported-by: Honggyu Kim <honggyu.kp@gmail.com>
Link: https://github.com/awslabs/damo/issues/75
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -173,13 +173,15 @@ def wops_for_schemes(ctx):
             'access_pattern': wops_for_scheme_access_pattern(
                 scheme.access_pattern, ctx),
             'action': scheme.action,
-            'apply_interval_us': '%d' % scheme.apply_interval_us,
             'quotas': wops_for_scheme_quotas(scheme.quotas),
             'watermarks': wops_for_scheme_watermarks(scheme.watermarks),
         }
         if feature_supported('schemes_filters'):
             schemes_wops[dirname]['filters'] = wops_for_scheme_filters(
                     scheme.filters)
+        if feature_supported('schemes_apply_interval'):
+            schemes_wops[dirname]['apply_interval_us'] = (
+                    '%d' % scheme.apply_interval_us)
     return schemes_wops
 
 def wops_for_regions(regions):
@@ -549,6 +551,9 @@ def update_supported_features():
 
     if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'filters')):
         feature_supports['schemes_filters'] = True
+
+    if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'apply_interval_us')):
+        feature_supports['schemes_apply_interval'] = True
 
     avail_ops, err = _avail_ops()
     if err == None:

_damon.py
@@ -882,6 +882,7 @@ features = ['record',       # was in DAMON patchset, but not merged in mainline
             'schemes_tried_regions',    # merged in v6.2-rc1
             'schemes_filters',          # merged in v6.3-rc1
             'schemes_tried_regions_sz', # developing
+            'schemes_apply_interval',   # developing on v6.5
             ]
 
 _damon_fs = None

tests/unit/test_damon_sysfs.py
@@ -24,6 +24,7 @@ class TestDamonSysfs(unittest.TestCase):
                 'schemes_tried_regions': True,
                 'schemes_wmarks': True,
                 'schemes_filters': True,
+                'schemes_apply_interval': True,
                 'vaddr': True}
 
         sysfs_dict = {

commit 4aae8a1cbc6d9816e1723546ed931d3501694337

_damon_sysfs: Fix wrong DAMOS apply interval feature check logic
The DAMOS apply interval feature check logic is checking if the file
exists using 'isdir()'.  Fix it.

Fixes: b5ae1c11b5dd ("_damon: Add schemes_apply_interval feature")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -550,7 +550,7 @@ def update_supported_features():
     if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'filters')):
         feature_supports['schemes_filters'] = True
 
-    if os.path.isdir(os.path.join(scheme_dir_of(0, 0, 0), 'apply_interval_us')):
+    if os.path.isfile(os.path.join(scheme_dir_of(0, 0, 0), 'apply_interval_us')):
         feature_supports['schemes_apply_interval'] = True
 
     avail_ops, err = _avail_ops()