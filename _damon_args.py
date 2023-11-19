9cd9747aad6ec573222ed0689d42f6ab966accfa
SeongJae Park <sj38.park@gmail.com>
2023-11-19 19:25:02 +0000

    _damon_args/commit_kdamonds(): Support quota goals only commit
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _damon_args.py
+++ _damon_args.py
@@ -376,11 +376,11 @@ def stage_kdamonds(args):
         return None, 'cannot apply kdamonds from args (%s)' % err
     return kdamonds, None
 
-def commit_kdamonds(args):
+def commit_kdamonds(args, commit_quota_goals_only):
     kdamonds, err = kdamonds_for(args)
     if err:
         return None, 'cannot create kdamonds to commit from args (%s)' % err
-    err = _damon.commit(kdamonds)
+    err = _damon.commit(kdamonds, commit_quota_goals_only)
     if err:
         return None, 'cannot commit kdamonds (%s)' % err
     return kdamonds, None
