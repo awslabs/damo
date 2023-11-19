awslabs/damo

commit 8f0204810fe7e33eadb3467529431bbc9c98c53b

_damon_sysfs: Support quota goals writing
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -131,10 +131,22 @@ def wops_for_scheme_watermarks(wmarks):
         'low': '%d' % wmarks.low_permil,
     }
 
+def wops_for_scheme_quota_goal(goal):
+    return {
+            'target_value': '%s' % goal.target_value_bp,
+            'current_value': '%s' % goal.current_value_bp,
+            }
+
+def wops_for_scheme_quota_goals(goals):
+    wops = {}
+    for idx, goal in enumerate(goals):
+        wops['%d' % idx] = wops_for_scheme_goal(goal)
+    return wops
+
 def wops_for_scheme_quotas(quotas):
     if quotas == None:
         return {}
-    return {
+    wops = {
         'ms': '%d' % quotas.time_ms,
         'bytes': '%d' % quotas.sz_bytes,
         'reset_interval_ms': '%d' % quotas.reset_interval_ms,
@@ -144,6 +156,10 @@ def wops_for_scheme_quotas(quotas):
             'age_permil': '%d' % quotas.weight_age_permil,
         },
     }
+    if feature_supported('schemes_quota_goals'):
+        wops['goals'] = wops_for_scheme_quota_goals(quotas.goals)
+
+    return wops
 
 def wops_for_scheme_access_pattern(pattern, ctx):
     if pattern == None:
@@ -247,6 +263,13 @@ def __ensure_scheme_dir_populated(scheme_dir, scheme):
         raise Exception('nr_filters read fail (%s)' % err)
     if int(nr_filters) != len(scheme.filters):
         _damo_fs.write_file(nr_filters_path, '%d' % len(scheme.filters))
+
+    nr_goals_path = os.path.join(scheme_dir, 'quotas', 'goals', 'nr_goals')
+    nr_goals, err = _damo_fs.read_file(nr_goals_path)
+    if err != None:
+        raise Exception('nr_goals read fail (%s)' % err)
+    if int(nr_goals) != len(scheme.quotas.goals):
+        _damo_fs.write_file(nr_goals_path, '%d' % len(scheme.quotas.goals))
 
 def __ensure_target_dir_populated(target_dir, target):
     nr_regions_path = os.path.join(target_dir, 'regions', 'nr_regions')

tests/unit/test_damon_sysfs.py
@@ -20,6 +20,7 @@ class TestDamonSysfs(unittest.TestCase):
                 'schemes': True,
                 'schemes_prioritization': True,
                 'schemes_quotas': True,
+                'schemes_quota_goals': True,
                 'schemes_speed_limit': True,
                 'schemes_tried_regions': True,
                 'schemes_wmarks': True,
@@ -116,6 +117,7 @@ class TestDamonSysfs(unittest.TestCase):
                                         "quotas": {
                                             "bytes": "0", "ms": "0",
                                             "reset_interval_ms": "0",
+                                            "goals": {},
                                             "weights": {
                                                 "age_permil": "0",
                                                 "nr_accesses_permil": "0",

commit 04b596cba261f29c6429c288f8747b5588092305

_damon_sysfs: Fix wrong function call
Fix a typo of function name.

Fixes: 8f0204810fe7 ("_damon_sysfs: Support quota goals writing")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -140,7 +140,7 @@ def wops_for_scheme_quota_goal(goal):
 def wops_for_scheme_quota_goals(goals):
     wops = {}
     for idx, goal in enumerate(goals):
-        wops['%d' % idx] = wops_for_scheme_goal(goal)
+        wops['%d' % idx] = wops_for_scheme_quota_goal(goal)
     return wops
 
 def wops_for_scheme_quotas(quotas):