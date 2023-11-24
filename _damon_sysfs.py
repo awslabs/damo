awslabs/damo

commit 0fb81fdd606890d1df065f878ed8a12604d4560a

Update the version
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -92,8 +92,10 @@ def update_schemes_tried_regions(kdamond_idxs):
     if not feature_supported('schemes_tried_regions'):
         return 'schemes_tried_regions not supported'
     for kdamond_idx in kdamond_idxs:
+        print('write file')
         err = _damo_fs.write_file(
                 state_file_of(kdamond_idx), 'update_schemes_tried_regions')
+        print('write returned')
         if err != None:
             return err
     return None

damo_version.py
@@ -1,1 +1,1 @@
-__version__ = '2.0.7'
+__version__ = '2.0.8'

commit 79a92f7817be627b6aaff8b4ae20ab28f49dd5c9

_damon_sysfs: Remove unnecessary debug log
The message for a debugging was mistakenly committed.  Remove.

Fixes: 0fb81fdd6068 ("Update the version")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_sysfs.py
@@ -92,10 +92,8 @@ def update_schemes_tried_regions(kdamond_idxs):
     if not feature_supported('schemes_tried_regions'):
         return 'schemes_tried_regions not supported'
     for kdamond_idx in kdamond_idxs:
-        print('write file')
         err = _damo_fs.write_file(
                 state_file_of(kdamond_idx), 'update_schemes_tried_regions')
-        print('write returned')
         if err != None:
             return err
     return None