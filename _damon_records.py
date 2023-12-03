awslabs/damo

commit 0eb3b4d087427b9d1da829f5c84a8d7a9c21b900

damo_show: Move more snapshot retrieval logics to _damon_records
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_records.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 
 import collections
+import copy
 import json
 import os
 import signal
@@ -737,3 +738,125 @@ def get_snapshot_records_for_schemes(idxs, total_sz_only, merge_regions):
 
     records = tried_regions_to_records_of(idxs, merge_regions)
     return records, None
+
+def filter_by_pattern(record, access_pattern):
+    sz_bytes = access_pattern.sz_bytes
+    nr_acc = access_pattern.nr_acc_min_max
+    age = access_pattern.age_min_max
+
+    for snapshot in record.snapshots:
+        filtered = []
+        for region in snapshot.regions:
+            sz = region.size()
+            if sz < sz_bytes[0] or sz_bytes[1] < sz:
+                continue
+            intervals = record.intervals
+            if intervals == None:
+                filtered.append(region)
+                continue
+            region.nr_accesses.add_unset_unit(intervals)
+            freq = region.nr_accesses.percent
+            if freq < nr_acc[0].percent or nr_acc[1].percent < freq:
+                continue
+            region.age.add_unset_unit(intervals)
+            usecs = region.age.usec
+            if usecs < age[0].usec or age[1].usec < usecs:
+                continue
+            filtered.append(region)
+        snapshot.regions = filtered
+
+def filter_by_addr(region, addr_ranges):
+    regions = []
+    for start, end in addr_ranges:
+        # out of the range
+        if region.end <= start or end <= region.start:
+            continue
+        # in the range
+        if start <= region.start and region.end <= end:
+            regions.append(copy.deepcopy(region))
+            continue
+        # overlap
+        copied = copy.deepcopy(region)
+        copied.start = max(start, region.start)
+        copied.end = min(end, region.end)
+        regions.append(copied)
+    return regions
+
+def filter_records_by_addr(records, addr_ranges):
+    for record in records:
+        for snapshot in record.snapshots:
+            filtered_regions = []
+            for region in snapshot.regions:
+                filtered_regions += filter_by_addr(region, addr_ranges)
+            snapshot.regions = filtered_regions
+            snapshot.update_total_bytes()
+
+def convert_addr_ranges_input(addr_ranges_input):
+    try:
+        ranges = [[_damo_fmt_str.text_to_bytes(start),
+            _damo_fmt_str.text_to_bytes(end)]
+            for start, end in addr_ranges_input]
+    except Exception as e:
+        return None, 'conversion to bytes failed (%s)' % e
+
+    ranges.sort(key=lambda x: x[0])
+    for idx, arange in enumerate(ranges):
+        start, end = arange
+        if start > end:
+            return None, 'start > end (%s)' % arange
+        if idx > 0 and ranges[idx - 1][1] > start:
+            return None, 'overlapping range'
+    return ranges, None
+
+def __get_records(access_pattern, address, tried_regions_of,
+        total_sz_only, dont_merge_regions):
+    err = 'assumed error'
+    nr_tries = 0
+    while err != None and nr_tries < 5:
+        nr_tries += 1
+        if tried_regions_of == None:
+            filters = []
+            if address and _damon.feature_supported('schemes_filters_addr'):
+                for start, end in address:
+                    filters.append(_damon.DamosFilter('addr', False,
+                        address_range=_damon.DamonRegion(start, end)))
+
+            monitor_scheme = _damon.Damos(access_pattern=access_pattern,
+                    filters=filters)
+
+            records, err = get_snapshot_records(monitor_scheme,
+                    total_sz_only, not dont_merge_regions)
+        else:
+             records, err = get_snapshot_records_for_schemes(
+                    tried_regions_of, total_sz_only, not dont_merge_regions)
+        if err != None:
+            time.sleep(random.randrange(
+                2**(nr_tries - 1), 2**nr_tries) / 100)
+    return records, err
+
+def get_records(input_file, access_pattern, address, tried_regions_of,
+        total_sz_only, dont_merge_regions):
+    address_filtered = False
+    if input_file == None:
+        records, err = __get_records(access_pattern, address, tried_regions_of,
+                total_sz_only, dont_merge_regions)
+        if err != None:
+            return None, err
+        if address and _damon.feature_supported('schemes_filters_addr'):
+            address_filtered = True
+    else:
+        if not os.path.isfile(input_file):
+            return None, '--input_file (%s) is not file' % input_file
+
+        records, err = parse_records_file(input_file)
+        if err:
+            return None, ('parsing damon result file (%s) failed (%s)' %
+                    (input_file, err))
+        for record in records:
+            filter_by_pattern(record, access_pattern)
+    if address and not address_filtered:
+        ranges, err = convert_addr_ranges_input(address)
+        if err:
+            return None, 'wrong --address input (%s)' % err
+        filter_records_by_addr(records, ranges)
+    return records, err

damo_show.py
@@ -1,12 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 
 import argparse
-import copy
 import json
 import math
-import os
-import random
-import time
 
 import _damo_ascii_color
 import _damo_fmt_str
@@ -481,128 +477,6 @@ def pr_records(args, records):
                 args.min_chars_for, None, None, None, record, args.raw_number,
                 region_box_args)
 
-def filter_by_pattern(record, access_pattern):
-    sz_bytes = access_pattern.sz_bytes
-    nr_acc = access_pattern.nr_acc_min_max
-    age = access_pattern.age_min_max
-
-    for snapshot in record.snapshots:
-        filtered = []
-        for region in snapshot.regions:
-            sz = region.size()
-            if sz < sz_bytes[0] or sz_bytes[1] < sz:
-                continue
-            intervals = record.intervals
-            if intervals == None:
-                filtered.append(region)
-                continue
-            region.nr_accesses.add_unset_unit(intervals)
-            freq = region.nr_accesses.percent
-            if freq < nr_acc[0].percent or nr_acc[1].percent < freq:
-                continue
-            region.age.add_unset_unit(intervals)
-            usecs = region.age.usec
-            if usecs < age[0].usec or age[1].usec < usecs:
-                continue
-            filtered.append(region)
-        snapshot.regions = filtered
-
-def filter_by_addr(region, addr_ranges):
-    regions = []
-    for start, end in addr_ranges:
-        # out of the range
-        if region.end <= start or end <= region.start:
-            continue
-        # in the range
-        if start <= region.start and region.end <= end:
-            regions.append(copy.deepcopy(region))
-            continue
-        # overlap
-        copied = copy.deepcopy(region)
-        copied.start = max(start, region.start)
-        copied.end = min(end, region.end)
-        regions.append(copied)
-    return regions
-
-def filter_records_by_addr(records, addr_ranges):
-    for record in records:
-        for snapshot in record.snapshots:
-            filtered_regions = []
-            for region in snapshot.regions:
-                filtered_regions += filter_by_addr(region, addr_ranges)
-            snapshot.regions = filtered_regions
-            snapshot.update_total_bytes()
-
-def convert_addr_ranges_input(addr_ranges_input):
-    try:
-        ranges = [[_damo_fmt_str.text_to_bytes(start),
-            _damo_fmt_str.text_to_bytes(end)]
-            for start, end in addr_ranges_input]
-    except Exception as e:
-        return None, 'conversion to bytes failed (%s)' % e
-
-    ranges.sort(key=lambda x: x[0])
-    for idx, arange in enumerate(ranges):
-        start, end = arange
-        if start > end:
-            return None, 'start > end (%s)' % arange
-        if idx > 0 and ranges[idx - 1][1] > start:
-            return None, 'overlapping range'
-    return ranges, None
-
-def __get_records(access_pattern, address, tried_regions_of,
-        total_sz_only, dont_merge_regions):
-    err = 'assumed error'
-    nr_tries = 0
-    while err != None and nr_tries < 5:
-        nr_tries += 1
-        if tried_regions_of == None:
-            filters = []
-            if address and _damon.feature_supported('schemes_filters_addr'):
-                for start, end in address:
-                    filters.append(_damon.DamosFilter('addr', False,
-                        address_range=_damon.DamonRegion(start, end)))
-
-            monitor_scheme = _damon.Damos(access_pattern=access_pattern,
-                    filters=filters)
-
-            records, err = _damon_records.get_snapshot_records(monitor_scheme,
-                    total_sz_only, not dont_merge_regions)
-        else:
-             records, err = _damon_records.get_snapshot_records_for_schemes(
-                    tried_regions_of, total_sz_only, not dont_merge_regions)
-        if err != None:
-            time.sleep(random.randrange(
-                2**(nr_tries - 1), 2**nr_tries) / 100)
-    return records, err
-
-def get_records(input_file, access_pattern, address, tried_regions_of,
-        total_sz_only, dont_merge_regions):
-    address_filtered = False
-    if input_file == None:
-        records, err = __get_records(access_pattern, address, tried_regions_of,
-                total_sz_only, dont_merge_regions)
-        if err != None:
-            return None, err
-        if address and _damon.feature_supported('schemes_filters_addr'):
-            address_filtered = True
-    else:
-        if not os.path.isfile(input_file):
-            return None, '--input_file (%s) is not file' % input_file
-
-        records, err = _damon_records.parse_records_file(input_file)
-        if err:
-            return None, ('parsing damon result file (%s) failed (%s)' %
-                    (input_file, err))
-        for record in records:
-            filter_by_pattern(record, access_pattern)
-    if address and not address_filtered:
-        ranges, err = convert_addr_ranges_input(address)
-        if err:
-            return None, 'wrong --address input (%s)' % err
-        filter_records_by_addr(records, ranges)
-    return records, err
-
 def handle_ls_keywords(args):
     if args.ls_record_format_keywords:
         print('\n\n'.join(['%s' % f for f in record_formatters]))
@@ -726,8 +600,9 @@ def main(args=None):
     if args.input_file == None:
         _damon.ensure_root_and_initialized(args, load_feature_supports=True)
 
-    records, err = get_records(args.input_file, access_pattern, args.address,
-            args.tried_regions_of, args.total_sz_only, args.dont_merge_regions)
+    records, err = _damon_records.get_records(args.input_file, access_pattern,
+            args.address, args.tried_regions_of, args.total_sz_only,
+            args.dont_merge_regions)
     if err != None:
         print(err)
         exit(1)

tests/unit/test_damo_show.py
@@ -12,41 +12,6 @@ import _damon_records
 import damo_show
 
 class TestDamoShow(unittest.TestCase):
-    def test_filter_by_address(self):
-        ranges = [[3, 5], [6, 9], [10, 12]]
-        region = _damon.DamonRegion(7, 8)
-        self.assertEqual(
-                damo_show.filter_by_addr(_damon.DamonRegion(7, 8), ranges),
-                [_damon.DamonRegion(7, 8)])
-        self.assertEqual(
-                damo_show.filter_by_addr(_damon.DamonRegion(1, 2), ranges),
-                [])
-        self.assertEqual(
-                damo_show.filter_by_addr(_damon.DamonRegion(5, 6), ranges),
-                [])
-        self.assertEqual(
-                damo_show.filter_by_addr(_damon.DamonRegion(1, 4), ranges),
-                [_damon.DamonRegion(3, 4)])
-        self.assertEqual(
-                damo_show.filter_by_addr(_damon.DamonRegion(1, 20), ranges),
-                [_damon.DamonRegion(3, 5), _damon.DamonRegion(6, 9),
-                    _damon.DamonRegion(10, 12)])
-
-    def test_convert_addr_ranges_input(self):
-        self.assertEqual(
-                damo_show.convert_addr_ranges_input([['1G', '2G']]),
-                ([[1024 * 1024 * 1024, 1024 * 1024 * 1024 * 2]], None))
-        ranges, err = damo_show.convert_addr_ranges_input([['abc', 'def']])
-        self.assertNotEqual(err, None)
-        ranges, err = damo_show.convert_addr_ranges_input([[4, 3]])
-        self.assertNotEqual(err, None)
-        ranges, err = damo_show.convert_addr_ranges_input([[5, 7], [2, 6]])
-        self.assertEqual(err, 'overlapping range')
-
-        self.assertEqual(
-                damo_show.convert_addr_ranges_input([[10, 20], [5, 7]]),
-                ([[5, 7], [10, 20]], None))
-
     def test_sorted_access_pattern(self):
         record = _damon_records.DamonRecord.from_kvpairs(
             {

tests/unit/test_damon_records.py
@@ -7,6 +7,7 @@ import _test_damo_common
 
 _test_damo_common.add_damo_dir_to_syspath()
 
+import _damon
 import _damon_records
 
 class TestDamon(unittest.TestCase):
@@ -96,6 +97,43 @@ class TestDamon(unittest.TestCase):
             ]]
         self.assertEqual(len(records), 1)
         self.assertEqual(len(records[0].snapshots), 1)
+
+    def test_filter_by_address(self):
+        ranges = [[3, 5], [6, 9], [10, 12]]
+        region = _damon.DamonRegion(7, 8)
+        self.assertEqual(
+                _damon_records.filter_by_addr(_damon.DamonRegion(7, 8),
+                    ranges), [_damon.DamonRegion(7, 8)])
+        self.assertEqual(
+                _damon_records.filter_by_addr(_damon.DamonRegion(1, 2),
+                    ranges), [])
+        self.assertEqual(
+                _damon_records.filter_by_addr(_damon.DamonRegion(5, 6),
+                    ranges), [])
+        self.assertEqual(
+                _damon_records.filter_by_addr(_damon.DamonRegion(1, 4),
+                    ranges), [_damon.DamonRegion(3, 4)])
+        self.assertEqual(
+                _damon_records.filter_by_addr(_damon.DamonRegion(1, 20),
+                    ranges), [_damon.DamonRegion(3, 5), _damon.DamonRegion(6,
+                        9), _damon.DamonRegion(10, 12)])
+
+    def test_convert_addr_ranges_input(self):
+        self.assertEqual(
+                _damon_records.convert_addr_ranges_input([['1G', '2G']]),
+                ([[1024 * 1024 * 1024, 1024 * 1024 * 1024 * 2]], None))
+        ranges, err = _damon_records.convert_addr_ranges_input(
+                [['abc', 'def']])
+        self.assertNotEqual(err, None)
+        ranges, err = _damon_records.convert_addr_ranges_input([[4, 3]])
+        self.assertNotEqual(err, None)
+        ranges, err = _damon_records.convert_addr_ranges_input(
+                [[5, 7], [2, 6]])
+        self.assertEqual(err, 'overlapping range')
+
+        self.assertEqual(
+                _damon_records.convert_addr_ranges_input([[10, 20], [5, 7]]),
+                ([[5, 7], [10, 20]], None))
 
 if __name__ == '__main__':
     unittest.main()

commit b444c79deb32ee3109818797e3e92be947cbbda1

_damon_records: Import random
Commit 0eb3b4d08742 ("damo_show: Move more snapshot retrieval logics to
_damon_records") moved random module using code to _damon_records.py,
but forgot importing the module.  Fix it.

Fixes: 0eb3b4d08742 ("damo_show: Move more snapshot retrieval logics to _damon_records")
Signed-off-by: SeongJae Park <sj38.park@gmail.com>

_damon_records.py
@@ -4,6 +4,7 @@ import collections
 import copy
 import json
 import os
+import random
 import signal
 import subprocess
 import time