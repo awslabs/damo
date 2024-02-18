dee1dca321db7d21f529315a624cc1df33eb249b
SeongJae Park <sj38.park@gmail.com>
2024-02-18 09:04:28 -0800

    damo_replay: Show the plan
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- damo_replay.py
+++ damo_replay.py
@@ -1,7 +1,43 @@
 # SPDX-License-Identifier: GPL-2.0
 
+import os
+
+import _damon_records
+import damo_record_info
+
+def replay_snapshot(snapshot):
+    for region in snapshot.regions:
+        print('access %s-%s with %s percent access rate for %s nanosecs' %
+              (region.start, region.end, region.nr_accesses.percent,
+               snapshot.end_time - snapshot.start_time))
+
 def main(args):
-    print('under construction')
+    input_file = args.input
+
+    if not os.path.isfile(input_file):
+        print('input file (%s) not exists' % input_file)
+        exit(1)
+
+    records, err = _damon_records.get_records(record_file=input_file)
+    if err:
+        print('parsing damon records file (%s) failed (%s)' %
+              (input_file, err))
+        exit(1)
+
+    if len(records) == 0:
+        print('no monitoring records in the file')
+        exit(1)
+
+    if len(records) != 1:
+        print('supporting only single record for now')
+
+    record = records[0]
+    for snapshot in record.snapshots:
+        for region in snapshot.regions:
+            region.nr_accesses.add_unset_unit(record.intervals)
+
+    for snapshot in record.snapshots:
+        replay_snapshot(snapshot)
 
 def set_argparser(parser):
     parser.add_argument('--input', metavar='<file>', default='damon.data',
