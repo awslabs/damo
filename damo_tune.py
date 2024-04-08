845859fc7f288cd632284fc398ea83b440ecbfeb
Mithun Veluri <velurimithun38@gmail.com>
2024-04-08 11:30:40 +0000

    damo_tune: Evaluate args before committing kdamonds
    - Check if user has provided any 'damos_action'. Without 'damos_action'
    tune will ignore damos_quotas and damos_quota_goal.
    
    - Throws error messages if no 'damos_action' is specified in tune cmd.
    
    Signed-off-by: Mithun Veluri <velurimithun38@gmail.com>

--- damo_tune.py
+++ damo_tune.py
@@ -7,11 +7,28 @@ Update DAMON input parameters.
 import _damon
 import _damon_args
 
+def evaluate_args_for_quota_goals(args):
+    if args.damos_action:
+        return True, None
+    
+    for key, value in args.__dict__.items():
+        if key.startswith('damos_') and len(value):
+            if key == 'damos_action': continue
+            return False, 'no \'damos_action\' provided in arguments while using --damos_* option(s)'
+
+    return True, None
+
 def main(args):
     _damon.ensure_root_and_initialized(args)
 
     if not _damon.any_kdamond_running():
         print('DAMON is not turned on')
+        exit(1)
+
+    correct_args, err = evaluate_args_for_quota_goals(args)
+
+    if (correct_args is not True and err is not None):
+        print('Tune error: incorrect arguments: %s' % err)
         exit(1)
 
     kdamonds, err = _damon_args.commit_kdamonds(args, args.quota_goals_only)
