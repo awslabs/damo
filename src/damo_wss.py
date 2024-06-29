be0a13c9641bde3a68eac9314a4e846cd7c0560a
SeongJae Park <sj@kernel.org>
2024-06-29 11:18:22 -0700

    damo_wss: Remove unused function, pr_wss_dists()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_wss.py
+++ src/damo_wss.py
@@ -37,48 +37,6 @@ def get_wss_dists(records, acc_thres, sz_thres, do_sort, collapse_targets):
         wss_dists = {0: collapsed_dist}
     return wss_dists
 
-def pr_wss_dists(wss_dists, percentiles, raw_number, nr_cols_bar, pr_all_wss):
-    print('# <percentile> <wss>')
-    for tid, wss_dist in wss_dists.items():
-        print('# target_id\t%s' % tid)
-        if len(wss_dist) == 0:
-            print('# no snapshot')
-            return
-        print('# avr:\t%s' % _damo_fmt_str.format_sz(
-            sum(wss_dist) / len(wss_dist), raw_number))
-
-        if pr_all_wss:
-            for idx, wss in enumerate(wss_dist):
-                print('%s %s' % (idx, _damo_fmt_str.format_sz(wss, raw_number)))
-            return
-
-        if nr_cols_bar > 0:
-            max_sz = 0
-            for percentile in percentiles:
-                wss_idx = int(percentile / 100.0 * len(wss_dist))
-                if wss_idx == len(wss_dist):
-                    wss_idx -= 1
-                wss = wss_dist[wss_idx]
-                if max_sz <= wss:
-                    max_sz = wss
-            if max_sz > 0:
-                sz_per_col = max_sz / nr_cols_bar
-            else:
-                sz_per_col = 1
-
-        for percentile in percentiles:
-            wss_idx = int(percentile / 100.0 * len(wss_dist))
-            if wss_idx == len(wss_dist):
-                wss_idx -= 1
-            wss = wss_dist[wss_idx]
-            line = '%3d %15s' % (percentile,
-                _damo_fmt_str.format_sz(wss, raw_number))
-            if nr_cols_bar > 0:
-                cols = int(wss / sz_per_col)
-                remaining_cols = nr_cols_bar - cols
-                line += ' |%s%s|' % ('*' * cols, ' ' * remaining_cols)
-            print(line)
-
 def set_argparser(parser):
     parser.add_argument('--input', '-i', type=str, metavar='<file>',
             default='damon.data', help='input file name')
