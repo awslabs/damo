3bd32168d30b0fab3362684523919bf02a4ca693
SeongJae Park <sj@kernel.org>
2024-06-29 10:58:04 -0700

    _damo_dist: Implement a function for general distributions printing
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/_damo_dist.py
+++ src/_damo_dist.py
@@ -21,3 +21,45 @@ def plot_dist(data_file, output_file, xlabel, ylabel):
     subprocess.call(['gnuplot', '-e', gnuplot_cmd])
     os.remove(data_file)
     return None
+
+def pr_dists(metric_name, dists, percentiles, pr_all, format_fn, raw_number,
+             nr_cols_bar):
+    '''
+    Print distributed metric values for given percentiles or all
+    '''
+    print('# <percentile> <%s>' % metric_name)
+    if len(dists) == 0:
+        print('# no snapshot')
+        return
+    print('# avr:\t%s' % format_fn(sum(dists) / len(dists), raw_number))
+
+    if pr_all:
+        for idx, val in enumerate(dists):
+            print('%s %s' % (idx, format_fn(val, raw_number)))
+        return
+
+    if nr_cols_bar > 0:
+        max_val = 0
+        for percentile in percentiles:
+            val_idx = int(percentile / 100.0 * len(dists))
+            if val_idx == len(dists):
+                val_idx -= 1
+            val = dists[val_idx]
+            if max_val <= val:
+                max_val = val
+        if max_val > 0:
+            val_per_col = max_val / nr_cols_bar
+        else:
+            val_per_col = 1
+
+    for percentile in percentiles:
+        idx = int(percentile / 100.0 * len(dists))
+        if idx == len(dists):
+            idx -= 1
+        val = dists[idx]
+        line = '%3d %15s' % (percentile, format_fn(val, raw_number))
+        if nr_cols_bar > 0:
+            cols = int(val / val_per_col)
+            remaining_cols = nr_cols_bar - cols
+            line += ' |%s%s|' % ('*' * cols, ' ' * remaining_cols)
+        print(line)
