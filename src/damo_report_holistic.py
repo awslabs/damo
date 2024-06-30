af2703ebe79c6ecd54d1321b56d661b9e1a3e729
SeongJae Park <sj@kernel.org>
2024-06-30 11:24:30 -0700

    damo_report_holistic: Print heatmaps for all regions of all recorded targets
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_report_holistic.py
+++ src/damo_report_holistic.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
 
+import argparse
+
 import _damo_records
+import damo_heatmap
 import damo_record_info
 
 def main(args):
@@ -12,13 +15,35 @@ def main(args):
               (args.access_pattern, err))
         exit(1)
 
+    guides = damo_record_info.get_guide_info(records)
+
     print('Overall recorded access pattern')
     print('===============================')
     print()
-    damo_record_info.pr_guide(records)
+    for guide in guides:
+        print(guide)
     print()
     print('# regions above are regions that access pattern recorded')
     print('# you can get this via \'damo record_info\', too')
+
+    print()
+    print('Heatmap')
+    print('=======')
+    print()
+    for guide in guides:
+        print('# target %d' % guide.tid)
+        for region in guide.regions():
+            print('# address range %d-%d' % (region[0], region[1]))
+            damo_heatmap.pr_heats(
+                    argparse.Namespace(
+                        tid=guide.tid, resol=[20, 80],
+                        time_range=[guide.start_time, guide.end_time],
+                        address_range=region,
+                        output='stdout',
+                        stdout_colorset='gray',
+                        stdout_skip_colorset_example=False,
+                        ),
+                    records)
 
 def set_argparser(parser):
     parser.add_argument(
