b0d70fbdf01cdf651f1f7e2d82fdb43d38f45ba5
SeongJae Park <sj@kernel.org>
2024-07-13 11:21:48 -0700

    damo_mk_damon_params: Support text format
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_mk_damon_params.py
+++ src/damo_mk_damon_params.py
@@ -5,16 +5,6 @@ import json
 import _damon
 import _damon_args
 
-def pr_json(kdamonds, raw):
-    for k in kdamonds:
-        for c in k.contexts:
-            for s in c.schemes:
-                s.stats = None
-                s.tried_regions = None
-
-    print(json.dumps({'kdamonds': [k.to_kvpairs(raw) for k in kdamonds]},
-                     indent=4))
-
 def main(args):
     _damon.ensure_root_permission()
 
@@ -23,15 +13,29 @@ def main(args):
         print('invalid arguments (%s)' % err)
         exit(1)
 
+    for k in kdamonds:
+        for c in k.contexts:
+            for s in c.schemes:
+                s.stats = None
+                s.tried_regions = None
+
     if args.format == 'json':
-        pr_json(kdamonds, args.raw)
+        print(json.dumps({'kdamonds':
+                          [k.to_kvpairs(args.raw) for k in kdamonds]},
+                         indent=4))
+    elif args.format == 'text':
+        for idx, k in enumerate(kdamonds):
+            print('kdamond %d' % idx)
+            text = k.to_str(args.raw)
+            for line in text.split('\n'):
+                print('    %s' % line)
 
 def set_argparser(parser):
     _damon_args.set_argparser(parser, add_record_options=False)
     parser.description = ' '.join([
         'format DAMON parameters'])
     parser.add_argument(
-            '--format', choices=['json'], default='json',
+            '--format', choices=['json', 'text'], default='json',
             help='format of the output')
     parser.add_argument(
             '--raw', action='store_true',
