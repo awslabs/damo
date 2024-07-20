d481e6672163faf7ddd58c004ab0680f93f4aa75
SeongJae Park <sj@kernel.org>
2024-07-20 12:22:59 -0700

    src/damo_args_damon: Support yaml format
    Suggested-by: Honggyu Kim <honggyu.kim@sk.com>
    Link: https://github.com/awslabs/damo/pull/97
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_args_damon.py
+++ src/damo_args_damon.py
@@ -1,11 +1,23 @@
 # SPDX-License-Identifier: GPL-2.0
 
 import json
+import sys
+
+try:
+    import yaml
+except ModuleNotFoundError as e:
+    # do nothing.  The yaml using functions should handle the exception
+    # properly.
+    pass
 
 import _damon
 import _damon_args
 
 def main(args):
+    if args.format == 'yaml':
+        if not 'yaml' in sys.modules:
+            print('yaml module import failed')
+            exit(1)
     _damon.ensure_root_permission()
 
     kdamonds, err = _damon_args.kdamonds_for(args)
@@ -23,13 +35,16 @@ def main(args):
         print(json.dumps({'kdamonds':
                           [k.to_kvpairs(args.raw) for k in kdamonds]},
                          indent=4))
+    elif args.format == 'yaml':
+        print(yaml.dump({'kdamonds':
+                          [k.to_kvpairs(args.raw) for k in kdamonds]}))
 
 def set_argparser(parser):
     _damon_args.set_argparser(parser, add_record_options=False)
     parser.description = ' '.join([
         'format DAMON parameters'])
     parser.add_argument(
-            '--format', choices=['json'], default='json',
+            '--format', choices=['json', 'yaml'], default='json',
             help='format of the output')
     parser.add_argument(
             '--raw', action='store_true',
