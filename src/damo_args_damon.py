2174c705efaaf928f161d68ecc592295fc790ea7
SeongJae Park <sj@kernel.org>
2024-07-28 10:14:17 -0700

    damo_args_damon: Print OrderedDict as normal dict for yaml
    yaml.dump() keeps collections.OrderedDict type on output.  But it only
    makes the output unnecessarily verbose.  Worse yet, yaml.safe_load(),
    which is used by _damon_args, cannot handle those.  Print the objects as
    normal dict on output while keeping the order.
    
    Reported-by: Honggyu Kim <honggyu.kim@sk.com>
    Fixes: https://github.com/awslabs/damo/issues/107
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_args_damon.py
+++ src/damo_args_damon.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 
+import collections
 import json
 import sys
 
@@ -12,6 +13,50 @@ except ModuleNotFoundError as e:
 
 import _damon
 import _damon_args
+
+'''
+kdamonds uses collections.OrderedDict.  yaml.dump() adds
+constructor comments for those like below.
+
+    kdamonds:
+    - !!python/object/apply:collections.OrderedDict
+      - - - state
+          - null
+        - - pid
+          - null
+
+yaml.safe_load(), which is used from _damon_args to read the yaml output again,
+fails at parsing it.  To support it, we should use yaml.load() with a specific
+loader.
+
+However, we use OrderedDict to make json or yaml-like dumped output keeps the
+order.  That's for letting it easy to read by human beings.  Here, the added
+constructor comment and slightly changed format are for machines.  Also
+from_kvpairs() methods for DAMON parameters don't really care about the orders
+and the type.
+
+So, adding the handling on dumping part instead of the loading part would make
+the result simpler while it all works seamlessly.  Use a custom dumper that
+prints OrderedDict as normal dict.
+'''
+def pr_kdamonds_yaml(kdamonds, raw):
+    def ordered_dict_representer(dumper, ordered_dict):
+        # represnet collections.OrderedDict object as normal dict, but while
+        # keeping the dumped order; the order will be ignored when loaded
+        # again, but that's not a problem for Kdamonds.
+        return dumper.represent_dict(ordered_dict.items())
+
+    class OrderedDumper(yaml.SafeDumper):
+        # Same to safe dumper but represent OrderedDict using
+        # ordered_dict_representer
+        pass
+
+    OrderedDumper.add_representer(
+            collections.OrderedDict, ordered_dict_representer)
+
+    print(yaml.dump(
+        {'kdamonds': [k.to_kvpairs(raw) for k in kdamonds]},
+        Dumper=OrderedDumper, default_flow_style=False, sort_keys=False))
 
 def main(args):
     if args.format == 'yaml':
@@ -36,8 +81,7 @@ def main(args):
                           [k.to_kvpairs(args.raw) for k in kdamonds]},
                          indent=4))
     elif args.format == 'yaml':
-        print(yaml.dump({'kdamonds':
-                          [k.to_kvpairs(args.raw) for k in kdamonds]}))
+        pr_kdamonds_yaml(kdamonds, args.raw)
 
 def set_argparser(parser):
     _damon_args.set_argparser(parser, add_record_options=False)
