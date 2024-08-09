fdfa10fcb66607ab80da40b00a65240e0a3ae57e
SeongJae Park <sj@kernel.org>
2024-08-09 11:27:44 -0700

    damo_record: Remove --footprint option
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_record.py
+++ src/damo_record.py
@@ -151,8 +151,6 @@ def set_argparser(parser):
     parser.add_argument('--no_footprint', action='store_false',
                         dest='footprint',
                         help='do not record memory footprint')
-    parser.add_argument('--footprint', action='store_true',
-                        help='record memory footprint')
     parser.add_argument('--no_vmas', action='store_false', dest='vmas',
                         help='record virtual memory areas (/proc/<pid>/maps)')
     parser.add_argument('--vmas', action='store_true',
