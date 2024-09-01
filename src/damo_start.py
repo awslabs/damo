99a23a9aa814138daf70d1d229a802e41104d9a0
SeongJae Park <sj@kernel.org>
2024-09-01 11:27:12 -0700

    damo_{record,start}: Stop using _damon_args.set_argparser() return value
    _damon_args.set_argparser() returns parser, but it is same to the given
    parameter.  So the return value is meaningless.  Cleanup code to not use
    the unnecessary return value.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- src/damo_start.py
+++ src/damo_start.py
@@ -16,6 +16,6 @@ def main(args):
         exit(1)
 
 def set_argparser(parser):
-    parser = _damon_args.set_argparser(parser, add_record_options=False)
+    _damon_args.set_argparser(parser, add_record_options=False)
     parser.description = 'Start DAMON with specified parameters'
     return parser
